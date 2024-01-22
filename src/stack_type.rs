f6c41211281c286252631d3ee3bf032c86e68552
Richard Neumann <neumann@paulmann.de>
2024-01-22 08:39:34 +0100

    Implement From<StackType> for u16

--- src/stack_type.rs
+++ src/stack_type.rs
@@ -1,6 +1,8 @@
+use num_derive::{FromPrimitive, ToPrimitive};
+use num_traits::{FromPrimitive, ToPrimitive};
 use std::fmt::{Display, Formatter};
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq, FromPrimitive, ToPrimitive)]
 pub enum StackType {
     Zigbee2006 = 0x0000,
     Zigbee2007 = 0x0001,
@@ -16,5 +18,13 @@ impl Display for StackType {
             Self::ZigbeePro => write!(f, "ZigBee Pro"),
             Self::ZigbeeIp => write!(f, "ZigBee IP"),
         }
+    }
+}
+
+impl From<StackType> for u16 {
+    fn from(stack_type: StackType) -> Self {
+        stack_type
+            .to_u16()
+            .expect("Could not convert StackType to u16")
     }
 }
