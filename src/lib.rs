c947948a04dac8e7458ca1267020d67774e98ddf
Richard Neumann <neumann@paulmann.de>
2024-01-22 08:37:14 +0100

    Implement status

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,1 @@
-
+mod status;
---
+++ src/status.rs
@@ -0,0 +1,14 @@
+#[derive(Debug, Eq, PartialEq)]
+pub enum Status {
+    Success,
+    Failure,
+    InvalidArguments,
+    NoResponse,
+    InvalidState,
+    Unsupported,
+    CommunicationError,
+    BadResponse,
+    NoResources,
+    FatalError,
+    NoNetwork,
+}
