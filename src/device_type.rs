b88a3bfcc904cad0f7975e6af6efae67d08bc726
Richard Neumann <neumann@paulmann.de>
2024-01-23 09:00:11 +0100

    Delegate conversion method

--- src/device_type.rs
+++ src/device_type.rs
@@ -17,8 +17,8 @@ pub enum DeviceType {
     SmartEnergy(SmartEnergy),
 }
 
-impl From<DeviceType> for ProfileType {
-    fn from(device_type: DeviceType) -> Self {
+impl From<&DeviceType> for ProfileType {
+    fn from(device_type: &DeviceType) -> Self {
         match device_type {
             DeviceType::HomeAutomation(_) => ProfileType::HomeAutomation,
             DeviceType::GreenPower(_) => ProfileType::GreenPower,
@@ -28,13 +28,8 @@ impl From<DeviceType> for ProfileType {
     }
 }
 
-impl From<&DeviceType> for ProfileType {
-    fn from(device_type: &DeviceType) -> Self {
-        match device_type {
-            DeviceType::HomeAutomation(_) => ProfileType::HomeAutomation,
-            DeviceType::GreenPower(_) => ProfileType::GreenPower,
-            DeviceType::LightLink(_) => ProfileType::LightLink,
-            DeviceType::SmartEnergy(_) => ProfileType::SmartEnergy,
-        }
+impl From<DeviceType> for ProfileType {
+    fn from(device_type: DeviceType) -> Self {
+        Self::from(&device_type)
     }
 }
