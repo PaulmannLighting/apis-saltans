ba8e48b189889bb5c96e5256d92da94dd1d7d9b3
Richard Neumann <neumann@paulmann.de>
2024-01-23 07:44:20 +0100

    [WIP] Implement endpoint

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,3 +1,17 @@
 mod address;
 
+use crate::node::Node;
 pub use address::Address;
+use std::collections::HashMap;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Endpoint {
+    node: Node,
+    endpoint_id: u8,
+    profile_id: u8,
+    device_id: u8,
+    device_version: u8,
+    input_clusters: HashMap<u8, Cluster>,
+    output_clusters: HashMap<u8, Cluster>,
+    applications: HashMap<u8, Application>,
+}
