4b68a1dfb2ce63f355691ad627334853e88c06cc
Richard Neumann <neumann@paulmann.de>
2024-12-16 10:53:52 +0100

    Implement basic methods

--- src/zcl/lighting/commands/move_to_hue.rs
+++ src/zcl/lighting/commands/move_to_hue.rs
@@ -7,6 +7,32 @@ pub struct MoveToHue {
     transition_time: u16,
 }
 
+impl MoveToHue {
+    #[must_use]
+    pub const fn new(hue: u8, direction: Direction, transition_time: u16) -> Self {
+        Self {
+            hue,
+            direction,
+            transition_time,
+        }
+    }
+
+    #[must_use]
+    pub const fn hue(&self) -> u8 {
+        self.hue
+    }
+
+    #[must_use]
+    pub const fn direction(&self) -> Direction {
+        self.direction
+    }
+
+    #[must_use]
+    pub const fn transition_time(&self) -> u16 {
+        self.transition_time
+    }
+}
+
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, FromPrimitive)]
 #[repr(u8)]
 pub enum Direction {
