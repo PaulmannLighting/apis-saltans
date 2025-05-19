4a44bd932d96526d3c036db9e3eb784d4254f796
Richard Neumann <neumann@paulmann.de>
2025-05-19 10:58:50 +0200

    Add enum variant docs

--- src/zcl/lighting/commands/move_hue.rs
+++ src/zcl/lighting/commands/move_hue.rs
@@ -39,8 +39,11 @@ impl Command for MoveHue {
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, FromPrimitive)]
 #[repr(u8)]
 pub enum Mode {
+    /// Stop move.
     Stop = 0x00,
+    /// Move up.
     Up = 0x01,
     // 0x02 is reserved.
+    /// Move down.
     Down = 0x03,
 }
--- src/zcl/lighting/commands/step_hue.rs
+++ src/zcl/lighting/commands/step_hue.rs
@@ -49,7 +49,9 @@ impl Command for StepHue {
 #[repr(u8)]
 pub enum Mode {
     // 0x00 is reserved.
+    /// Step up.
     Up = 0x01,
     // 0x02 is reserved.
+    /// Step down.
     Down = 0x03,
 }
