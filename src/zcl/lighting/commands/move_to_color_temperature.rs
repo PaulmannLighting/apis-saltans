2db8d8771df40ead97f092ac4094cc0848d5fe67
Richard Neumann <neumann@paulmann.de>
2025-07-14 13:12:38 +0200

    Implement MoveToColorTemperature

---
+++ src/zcl/lighting/commands/move_to_color_temperature.rs
@@ -0,0 +1,41 @@
+use std::time::Duration;
+
+use crate::zcl::{Cluster, Command, constants::DECI_SECONDS_PER_MILLISECOND};
+
+/// Command to move a light's color temperature to a specific value in mireds.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct MoveToColorTemperature {
+    mireds: u16,
+    transition_time: u16,
+}
+
+impl MoveToColorTemperature {
+    /// Create a new `MoveToColorTemperature` command.
+    #[must_use]
+    pub const fn new(mireds: u16, transition_time: u16) -> Self {
+        Self {
+            mireds,
+            transition_time,
+        }
+    }
+
+    /// Return the color temperature in mireds.
+    #[must_use]
+    pub const fn mireds(self) -> u16 {
+        self.mireds
+    }
+
+    /// Return the transition time.
+    #[must_use]
+    pub fn transition_time(self) -> Duration {
+        Duration::from_millis(u64::from(self.transition_time) * DECI_SECONDS_PER_MILLISECOND)
+    }
+}
+
+impl Cluster for MoveToColorTemperature {
+    const ID: u16 = 0x0300;
+}
+
+impl Command for MoveToColorTemperature {
+    const ID: u8 = 0x0a;
+}
