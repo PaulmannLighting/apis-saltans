498b4695c32fbfb0c3b8f39f5f8aadd7d8a0d564
Richard Neumann <neumann@paulmann.de>
2025-07-14 14:05:16 +0200

    Implement EnhancedMoveToHueAndSaturation

---
+++ src/zcl/lighting/commands/enhanced_move_to_hue_and_saturation.rs
@@ -0,0 +1,49 @@
+use std::time::Duration;
+
+use crate::zcl::{Cluster, Command, constants::DECI_SECONDS_PER_MILLISECOND};
+
+/// Command to move a light to a specific hue and saturation with enhanced precision.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct EnhancedMoveToHueAndSaturation {
+    enhanced_hue: u16,
+    saturation: u8,
+    transition_time: u16,
+}
+
+impl EnhancedMoveToHueAndSaturation {
+    /// Create a new `EnhancedMoveToHueAndSaturation` command.
+    #[must_use]
+    pub const fn new(enhanced_hue: u16, saturation: u8, transition_time: u16) -> Self {
+        Self {
+            enhanced_hue,
+            saturation,
+            transition_time,
+        }
+    }
+
+    /// Return the enhanced hue value.
+    #[must_use]
+    pub const fn enhanced_hue(self) -> u16 {
+        self.enhanced_hue
+    }
+
+    /// Return the saturation value.
+    #[must_use]
+    pub const fn saturation(self) -> u8 {
+        self.saturation
+    }
+
+    /// Return the transition time.
+    #[must_use]
+    pub fn transition_time(self) -> Duration {
+        Duration::from_millis(u64::from(self.transition_time) * DECI_SECONDS_PER_MILLISECOND)
+    }
+}
+
+impl Cluster for EnhancedMoveToHueAndSaturation {
+    const ID: u16 = 0x0300;
+}
+
+impl Command for EnhancedMoveToHueAndSaturation {
+    const ID: u8 = 0x43;
+}
