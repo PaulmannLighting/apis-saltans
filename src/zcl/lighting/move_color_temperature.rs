a4c8f401c3399d165c40b52e28eec324078d5f0e
Richard Neumann <neumann@paulmann.de>
2025-07-15 08:50:20 +0200

    Implement MoveColorTemperature

---
+++ src/zcl/lighting/move_color_temperature.rs
@@ -0,0 +1,60 @@
+use crate::zcl::Command;
+use crate::zcl::lighting::Lighting;
+use crate::zcl::lighting::move_hue::Mode;
+
+/// Command to move a light's color temperature.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct MoveColorTemperature {
+    mode: Mode,
+    rate: u16,
+    color_temp_min_mireds: u16,
+    color_temp_max_mireds: u16,
+}
+
+impl MoveColorTemperature {
+    /// Create a new `MoveColorTemperature` command.
+    #[must_use]
+    pub const fn new(
+        mode: Mode,
+        rate: u16,
+        color_temp_min_mireds: u16,
+        color_temp_max_mireds: u16,
+    ) -> Self {
+        Self {
+            mode,
+            rate,
+            color_temp_min_mireds,
+            color_temp_max_mireds,
+        }
+    }
+
+    /// Return the mode of color temperature movement.
+    #[must_use]
+    pub const fn mode(self) -> Mode {
+        self.mode
+    }
+
+    /// Return the rate of color temperature change in mireds per second.
+    #[must_use]
+    pub const fn rate(self) -> u16 {
+        self.rate
+    }
+
+    /// Return the minimum color temperature in mireds.
+    #[must_use]
+    pub const fn color_temp_min_mireds(self) -> u16 {
+        self.color_temp_min_mireds
+    }
+
+    /// Return the maximum color temperature in mireds.
+    #[must_use]
+    pub const fn color_temp_max_mireds(self) -> u16 {
+        self.color_temp_max_mireds
+    }
+}
+
+impl Lighting for MoveColorTemperature {}
+
+impl Command for MoveColorTemperature {
+    const ID: u8 = 0x4b;
+}
