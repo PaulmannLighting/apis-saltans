001c85e0ccb0ac90639701feff3872e6db1f8da2
Richard Neumann <neumann@paulmann.de>
2025-07-15 17:21:54 +0200

    Add IDs to DataType

--- src/zcl/data_type.rs
+++ src/zcl/data_type.rs
@@ -12,75 +12,77 @@ mod structure;
 mod time_of_day;
 mod utc_time;
 
+/// Available Zigbee data types.
 #[derive(Clone, Debug, PartialEq, PartialOrd)]
+#[repr(u8)]
 pub enum DataType {
-    Null,
-    /// Generic
-    Data8(u8),
-    Data16([u8; 2]),
-    Data24([u8; 3]),
-    Data32([u8; 4]),
-    Data40([u8; 5]),
-    Data48([u8; 6]),
-    Data56([u8; 7]),
-    Data64([u8; 8]),
-    Logical(bool),
-    /// Bitmap
-    Map8(u8),
-    Map16(u16),
-    Map24(u32),
-    Map32(u32),
-    Map40(u64),
-    Map48(u64),
-    Map56(u64),
-    Map64(u64),
-    /// Unsigned integer
-    Uint8(u8),
-    Uint16(u16),
-    Uint24(u32),
-    Uint32(u32),
-    Uint40(u64),
-    Uint48(u64),
-    Uint56(u64),
-    Uint64(u64),
-    /// Signed integer
-    Int8(i8),
-    Int16(i16),
-    Int24(i32),
-    Int32(i32),
-    Int40(i64),
-    Int48(i64),
-    Int56(i64),
-    Int64(i64),
-    /// Enumeration
-    Enum8(u8),
-    Enum16(u16),
-    /// Floating point
-    Semi(f16),
-    Single(f32),
-    Double(f64),
-    /// String
-    OctStr(Vec<u8>),
-    String(String),
-    OctStr16(Vec<u8>),
-    String16(String),
-    /// Ordered sequence
-    Array(Array),
-    Structure(Structure),
-    /// Collection
-    Set(Vec<u8>),
-    Bag(Vec<u8>),
-    /// Time
-    TimeOfDay(TimeOfDay),
-    Date(Date),
-    UtcTime(UtcTime),
-    /// Identifier
-    ClusterId(u16),
-    AttributeId(u16),
-    BacNetOid(u32),
-    /// Miscellaneous
-    IeeeAddress(MacAddr8),
-    SecurityKey([u8; 16]),
-    Opaque(Vec<u8>),
-    Unknown,
+    Null = 0x00,
+    // Generic
+    Data8(u8) = 0x08,
+    Data16([u8; 2]) = 0x09,
+    Data24([u8; 3]) = 0x0a,
+    Data32([u8; 4]) = 0x0b,
+    Data40([u8; 5]) = 0x0c,
+    Data48([u8; 6]) = 0x0d,
+    Data56([u8; 7]) = 0x0e,
+    Data64([u8; 8]) = 0x0f,
+    Logical(bool) = 0x10,
+    // Bitmap
+    Map8(u8) = 0x18,
+    Map16(u16) = 0x19,
+    Map24(u32) = 0x1a,
+    Map32(u32) = 0x1b,
+    Map40(u64) = 0x1c,
+    Map48(u64) = 0x1d,
+    Map56(u64) = 0x1e,
+    Map64(u64) = 0x1f,
+    // Unsigned integer
+    Uint8(u8) = 0x20,
+    Uint16(u16) = 0x21,
+    Uint24(u32) = 0x22,
+    Uint32(u32) = 0x23,
+    Uint40(u64) = 0x24,
+    Uint48(u64) = 0x25,
+    Uint56(u64) = 0x26,
+    Uint64(u64) = 0x27,
+    // Signed integer
+    Int8(i8) = 0x28,
+    Int16(i16) = 0x29,
+    Int24(i32) = 0x2a,
+    Int32(i32) = 0x2b,
+    Int40(i64) = 0x2c,
+    Int48(i64) = 0x2d,
+    Int56(i64) = 0x2e,
+    Int64(i64) = 0x2f,
+    // Enumeration
+    Enum8(u8) = 0x30,
+    Enum16(u16) = 0x31,
+    // Floating point
+    Semi(f16) = 0x38,
+    Single(f32) = 0x39,
+    Double(f64) = 0x3a,
+    // String
+    OctStr(Vec<u8>) = 0x41,
+    String(String) = 0x42,
+    OctStr16(Vec<u8>) = 0x43,
+    String16(String) = 0x44,
+    // Ordered sequence
+    Array(Array) = 0x48,
+    Structure(Structure) = 0x4c,
+    // Collection
+    Set(Vec<u8>) = 0x50,
+    Bag(Vec<u8>) = 0x51,
+    // Time
+    TimeOfDay(TimeOfDay) = 0xe0,
+    Date(Date) = 0xe1,
+    UtcTime(UtcTime) = 0xe2,
+    // Identifier
+    ClusterId(u16) = 0xe8,
+    AttributeId(u16) = 0xe9,
+    BacNetOid(u32) = 0xea,
+    // Miscellaneous
+    IeeeAddress(MacAddr8) = 0xf0,
+    SecurityKey([u8; 16]) = 0xf1,
+    Opaque(Vec<u8>) = 0xf2,
+    Unknown = 0xff,
 }
