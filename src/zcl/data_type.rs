e133869d92b0cadf5ff77f05d8852b937bbe28ae
Richard Neumann <neumann@paulmann.de>
2025-07-15 17:27:27 +0200

    Add documentation for boolean

--- src/zcl/data_type.rs
+++ src/zcl/data_type.rs
@@ -35,7 +35,7 @@ pub enum DataType {
     Data56([u8; 7]) = 0x0e,
     /// 64-bit data.
     Data64([u8; 8]) = 0x0f,
-    // Boolean
+    /// Boolean.
     Logical(bool) = 0x10,
     // Bitmap
     /// 8-bit bitmap.
