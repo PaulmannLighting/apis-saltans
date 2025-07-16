c88dbe1e2a0a0722aa21429354748dea10e016d5
Richard Neumann <neumann@paulmann.de>
2025-07-16 09:24:15 +0200

    Add docstrings to attributes

--- src/zcl/basic/attributes.rs
+++ src/zcl/basic/attributes.rs
@@ -12,18 +12,32 @@ mod power_source;
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[repr(u16)]
 pub enum Attribute {
+    /// The ZCL version.
     ZCLVersion(u8) = 0x0000,
+    /// The application version.
     ApplicationVersion(u8) = 0x0001,
+    /// The stack version.
     StackVersion(u8) = 0x0002,
+    /// The hardware version.
     HWVersion(u8) = 0x0003,
+    /// The manufacturer name.
     ManufacturerName(heapless::String<32>) = 0x0004,
+    /// The model identifier.
     ModelIdentifier(heapless::String<32>) = 0x0005,
+    /// The date code.
     DateCode(DateCode) = 0x0006,
+    /// The power source.
     PowerSource(PowerSource) = 0x0007,
+    /// The generic device class.
     LocationDescription(heapless::String<16>) = 0x0010,
+    /// The physical environment.
     PhysicalEnvironment(PhysicalEnvironment) = 0x0011,
+    /// The device enabled state.
     DeviceEnabled(DeviceEnabled) = 0x0012,
+    /// The alarm mask.
     AlarmMask(u8) = 0x0013,
+    /// The disable local configuration attribute.
     DisableLocalConfig(u8) = 0x0014,
+    /// The cluster revision.
     SwBuildId(heapless::String<16>) = 0x4000,
 }
