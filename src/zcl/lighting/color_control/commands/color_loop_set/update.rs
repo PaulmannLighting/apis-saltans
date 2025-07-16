4b6bc7a0f7bad266a69b166eaa2c1bb7ac7c7428
Richard Neumann <neumann@paulmann.de>
2025-07-16 11:16:40 +0200

    Fix endianness

--- src/zcl/lighting/color_control/commands/color_loop_set/update.rs
+++ src/zcl/lighting/color_control/commands/color_loop_set/update.rs
@@ -7,12 +7,12 @@ pub struct Update(u8);
 bitflags! {
     impl Update: u8 {
         /// Flag whether to adhere to the action field.
-        const UPDATE_ACTION = 0b1000_0000;
+        const UPDATE_ACTION = 0b0000_0001;
         /// Flag whether to update the `ColorLoopDirection` attribute.
-        const UPDATE_DIRECTION = 0b0100_0000;
+        const UPDATE_DIRECTION = 0b0000_0010;
         /// Flag whether to update the `ColorLoopTime` attribute.
-        const UPDATE_TIME = 0b0010_0000;
+        const UPDATE_TIME = 0b0000_0100;
         /// Flag whether to update the `ColorLoopStartEnhancedHue` attribute.
-        const UPDATE_START_HUE = 0b0001_0000;
+        const UPDATE_START_HUE = 0b0000_1000;
     }
 }
