26983eb6758d1f2e093e265ead2b1fa225fb1dcb
Richard Neumann <neumann@paulmann.de>
2025-07-16 10:31:54 +0200

    Add some tests

--- src/zcl/basic/attributes/device_enabled.rs
+++ src/zcl/basic/attributes/device_enabled.rs
@@ -29,3 +29,43 @@ impl ToLeStream for DeviceEnabled {
         (self as u8).to_le_stream()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_device_enabled_from_le_stream() {
+        let bytes = vec![0x01];
+        let device_enabled = DeviceEnabled::from_le_stream(bytes.into_iter()).unwrap();
+        assert_eq!(device_enabled, DeviceEnabled::Enabled);
+    }
+
+    #[test]
+    fn test_device_disabled_from_le_stream() {
+        let bytes = vec![0x00];
+        let device_enabled = DeviceEnabled::from_le_stream(bytes.into_iter()).unwrap();
+        assert_eq!(device_enabled, DeviceEnabled::Disabled);
+    }
+
+    #[test]
+    fn test_device_enabled_from_le_stream_invalid() {
+        let bytes = vec![0x02];
+        let device_enabled = DeviceEnabled::from_le_stream(bytes.into_iter());
+        assert!(device_enabled.is_none());
+    }
+
+    #[test]
+    fn test_device_enabled_to_le_stream() {
+        let device_enabled = DeviceEnabled::Enabled;
+        let bytes: Vec<u8> = device_enabled.to_le_stream().collect();
+        assert_eq!(bytes, vec![0x01]);
+    }
+
+    #[test]
+    fn test_device_disabled_to_le_stream() {
+        let device_enabled = DeviceEnabled::Disabled;
+        let bytes: Vec<u8> = device_enabled.to_le_stream().collect();
+        assert_eq!(bytes, vec![0x00]);
+    }
+}
