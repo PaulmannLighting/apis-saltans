dcd8b63fe9433f938eadd35dc56cfda159bcbdb7
Richard Neumann <neumann@paulmann.de>
2025-07-17 09:16:40 +0200

    Use repr_discriminant

--- src/zcl/power_configuration/attribute/mains_information.rs
+++ src/zcl/power_configuration/attribute/mains_information.rs
@@ -1,22 +1,10 @@
-use std::ptr::from_ref;
+use repr_discriminant::repr_discriminant;
 
 /// Mains Information attribute.
-#[repr(u16)]
+#[repr_discriminant(u16)]
 pub enum MainsInformation {
     /// Mains voltage in 100mV.
     MainsVoltage(u16) = 0x0000,
     /// Mains frequency in Hertz.
     MainsFrequency(u8) = 0x0001,
-}
-
-impl MainsInformation {
-    /// Return the enum ID.
-    #[must_use]
-    pub const fn id(&self) -> u16 {
-        // SAFETY: This is safe because `MainsInformation` is repr(u16).
-        #[allow(unsafe_code)]
-        unsafe {
-            *from_ref::<Self>(self).cast::<u16>()
-        }
-    }
 }
