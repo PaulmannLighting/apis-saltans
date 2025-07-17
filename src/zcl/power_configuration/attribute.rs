138db36e7bf355a2798317b4694af9a592f79b00
Richard Neumann <neumann@paulmann.de>
2025-07-17 09:26:07 +0200

    Parse MainsInformation

--- src/zcl/power_configuration/attribute.rs
+++ src/zcl/power_configuration/attribute.rs
@@ -1,3 +1,4 @@
+use le_stream::FromLeStream;
 pub use mains_information::MainsInformation;
 use repr_discriminant::repr_discriminant;
 
@@ -24,4 +25,25 @@ pub enum Attribute {
     BatterySource3Information = 0x0060,
     /// Battery source 3 settings.
     BatterySource3Settings = 0x0070,
+}
+
+impl FromLeStream for Attribute {
+    fn from_le_stream<T>(mut bytes: T) -> Option<Self>
+    where
+        T: Iterator<Item = u8>,
+    {
+        let id = u16::from_le_stream(&mut bytes)?;
+        match id & ATTRIBUTE_ID_MASK {
+            0x0000 => MainsInformation::from_le_stream(id & ATTRIBUTE_VALUE_MASK, &mut bytes)
+                .map(Self::MainsInformation),
+            0x0010 => Some(Self::MainsSettings),
+            0x0020 => Some(Self::BatteryInformation),
+            0x0030 => Some(Self::BatterySettings),
+            0x0040 => Some(Self::BatterySource2Information),
+            0x0050 => Some(Self::BatterySource2Settings),
+            0x0060 => Some(Self::BatterySource3Information),
+            0x0070 => Some(Self::BatterySource3Settings),
+            _ => None,
+        }
+    }
 }
--- src/zcl/power_configuration/attribute/mains_information.rs
+++ src/zcl/power_configuration/attribute/mains_information.rs
@@ -1,3 +1,4 @@
+use le_stream::FromLeStream;
 use repr_discriminant::repr_discriminant;
 
 /// Mains Information attribute.
@@ -7,4 +8,17 @@ pub enum MainsInformation {
     MainsVoltage(u16) = 0x0000,
     /// Mains frequency in Hertz.
     MainsFrequency(u8) = 0x0001,
+}
+
+impl MainsInformation {
+    pub(crate) fn from_le_stream<T>(id: u16, mut bytes: T) -> Option<Self>
+    where
+        T: Iterator<Item = u8>,
+    {
+        match id {
+            0x0000 => u16::from_le_stream(&mut bytes).map(Self::MainsVoltage),
+            0x0001 => u8::from_le_stream(&mut bytes).map(Self::MainsFrequency),
+            _ => None,
+        }
+    }
 }
