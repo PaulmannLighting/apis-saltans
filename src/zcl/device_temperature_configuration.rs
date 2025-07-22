359b3f2bf9a690e88ac34ea3edd30023e871e2ca
Richard Neumann <neumann@paulmann.de>
2025-07-22 12:41:57 +0200

    Implement Temperature

--- src/zcl/device_temperature_configuration.rs
+++ src/zcl/device_temperature_configuration.rs
@@ -2,8 +2,10 @@
 
 pub use attribute::Attribute;
 pub use device_temp_alarm_mask::DeviceTempAlarmMask;
+pub use temperature::Temperature;
 
 mod attribute;
 mod device_temp_alarm_mask;
+mod temperature;
 
 const CLUSTER_ID: u16 = 0x0002;
---
+++ src/zcl/device_temperature_configuration/temperature.rs
@@ -0,0 +1,33 @@
+use core::ops::RangeInclusive;
+
+const RANGE: RangeInclusive<i16> = -200..=200;
+const INVALID_READING: i16 = -1; // 0xffff in unsigned representation
+
+/// Represents a temperature in degrees Celsius, with handling for valid, out-of-range, and invalid readings.
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum Temperature {
+    Valid(i16),
+    OutOfRange(i16),
+    InvalidReading,
+}
+
+impl From<i16> for Temperature {
+    fn from(value: i16) -> Self {
+        if value == INVALID_READING {
+            Self::InvalidReading
+        } else if RANGE.contains(&value) {
+            Self::Valid(value)
+        } else {
+            Self::OutOfRange(value)
+        }
+    }
+}
+
+impl From<Temperature> for i16 {
+    fn from(temp: Temperature) -> Self {
+        match temp {
+            Temperature::OutOfRange(value) | Temperature::Valid(value) => value,
+            Temperature::InvalidReading => INVALID_READING,
+        }
+    }
+}
