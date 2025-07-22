ba22d3299ddcb53c429f2a166a96fa91d61a25f8
Richard Neumann <neumann@paulmann.de>
2025-07-22 16:06:09 +0200

    Derive FromLeStreamTagged where appropriate

--- src/zcl/device_temperature_configuration/attribute.rs
+++ src/zcl/device_temperature_configuration/attribute.rs
@@ -1,15 +1,18 @@
 use core::iter::Chain;
 
 use intx::U24;
-use le_stream::{FromLeStream, FromLeStreamTagged, ToLeStream};
+use le_stream::ToLeStream;
+use le_stream::derive::FromLeStreamTagged;
 use repr_discriminant::repr_discriminant;
 
 use crate::util::DeviceTemperatureConfigurationAttributeIterator;
 use crate::zcl::device_temperature_configuration::DeviceTempAlarmMask;
 
 /// Attributes for the Device Temperature Configuration cluster.
+
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[repr_discriminant(u16, id)]
+#[derive(FromLeStreamTagged)]
 pub enum Attribute {
     // Device Temperature Information.
     /// Current temperature in degrees Celsius.
@@ -31,28 +34,6 @@ pub enum Attribute {
     LowTempDwellTripPoint(U24) = 0x0013,
     /// High temperature dwell trip point in seconds.
     HighTempDwellTripPoint(U24) = 0x0014,
-}
-
-impl FromLeStreamTagged for Attribute {
-    type Tag = u16;
-
-    fn from_le_stream_tagged<T>(tag: Self::Tag, bytes: T) -> Result<Option<Self>, Self::Tag>
-    where
-        T: Iterator<Item = u8>,
-    {
-        match tag {
-            0x0000 => Ok(i16::from_le_stream(bytes).map(Self::CurrentTemperature)),
-            0x0001 => Ok(i16::from_le_stream(bytes).map(Self::MinTempExperienced)),
-            0x0002 => Ok(i16::from_le_stream(bytes).map(Self::MaxTempExperienced)),
-            0x0003 => Ok(u16::from_le_stream(bytes).map(Self::OverTempTotalDwell)),
-            0x0010 => Ok(DeviceTempAlarmMask::from_le_stream(bytes).map(Self::DeviceTempAlarmMask)),
-            0x0011 => Ok(i16::from_le_stream(bytes).map(Self::LowTempThreshold)),
-            0x0012 => Ok(i16::from_le_stream(bytes).map(Self::HighTempThreshold)),
-            0x0013 => Ok(U24::from_le_stream(bytes).map(Self::LowTempDwellTripPoint)),
-            0x0014 => Ok(U24::from_le_stream(bytes).map(Self::HighTempDwellTripPoint)),
-            unknown => Err(unknown),
-        }
-    }
 }
 
 impl ToLeStream for Attribute {
--- src/zcl/identify/attribute.rs
+++ src/zcl/identify/attribute.rs
@@ -1,28 +1,16 @@
 use core::iter::Chain;
 
-use le_stream::{FromLeStream, FromLeStreamTagged, ToLeStream};
+use le_stream::ToLeStream;
+use le_stream::derive::FromLeStreamTagged;
 use repr_discriminant::repr_discriminant;
 
 /// Attributes for the Identify cluster.
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[repr_discriminant(u16, id)]
+#[derive(FromLeStreamTagged)]
 pub enum Attribute {
     /// Remaining length of time, in seconds, that the device will continue to identify itself.
     IdentifyTime(u16) = 0x0000,
-}
-
-impl FromLeStreamTagged for Attribute {
-    type Tag = u16;
-
-    fn from_le_stream_tagged<T>(tag: Self::Tag, bytes: T) -> Result<Option<Self>, Self::Tag>
-    where
-        T: Iterator<Item = u8>,
-    {
-        match tag {
-            0x0000 => Ok(u16::from_le_stream(bytes).map(Self::IdentifyTime)),
-            unknown => Err(unknown),
-        }
-    }
 }
 
 impl ToLeStream for Attribute {
