c6fd15d5c1d27efa9a1ac90d1db535fc1df8e6a1
Richard Neumann <neumann@paulmann.de>
2025-07-22 12:42:21 +0200

    Add docstrings

--- src/zcl/device_temperature_configuration/temperature.rs
+++ src/zcl/device_temperature_configuration/temperature.rs
@@ -6,8 +6,11 @@ const INVALID_READING: i16 = -1; // 0xffff in unsigned representation
 /// Represents a temperature in degrees Celsius, with handling for valid, out-of-range, and invalid readings.
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum Temperature {
+    /// A valid temperature reading within the defined range.
     Valid(i16),
+    /// An out-of-range temperature reading.
     OutOfRange(i16),
+    /// An invalid temperature reading.
     InvalidReading,
 }
 
