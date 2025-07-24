83e6f28228b539b597a16c462edfcd25a9ee93d7
Richard Neumann <neumann@paulmann.de>
2025-07-24 09:08:48 +0200

    Implement Cluster and Command for IdentifyQuery.

--- src/zcl/identify/commands/identify_query.rs
+++ src/zcl/identify/commands/identify_query.rs
@@ -1,3 +1,14 @@
+use crate::zcl::identify::CLUSTER_ID;
+use crate::zcl::{Cluster, Command};
+
 /// Request the target to respond if they are currently identifying themselves.
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct IdentifyQuery;
+
+impl Cluster for IdentifyQuery {
+    const ID: u16 = CLUSTER_ID;
+}
+
+impl Command for IdentifyQuery {
+    const ID: u8 = 0x01;
+}
