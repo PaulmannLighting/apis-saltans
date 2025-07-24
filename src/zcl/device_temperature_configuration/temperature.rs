a786369a116bca4fd7e571f7f578c9eb40aac184
Richard Neumann <neumann@paulmann.de>
2025-07-24 13:45:17 +0200

    Make Temperature a struct

--- src/zcl/device_temperature_configuration/temperature.rs
+++ src/zcl/device_temperature_configuration/temperature.rs
@@ -1,58 +1,32 @@
 use core::ops::RangeInclusive;
 
-use le_stream::{FromLeStream, ToLeStream};
+use le_stream::derive::{FromLeStream, ToLeStream};
 
 const INVALID_READING: i16 = -1; // 0xffff in unsigned representation as per 3.4.2.2.1.1.
 const RANGE: RangeInclusive<i16> = -200..=200;
 
 /// Represents a temperature in degrees Celsius, with handling for valid, out-of-range, and invalid readings.
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub enum Temperature {
-    /// A valid temperature reading within the defined range in °C.
-    Valid(i16),
-    /// An out-of-range temperature reading.
-    OutOfRange(i16),
-    /// An invalid temperature reading.
-    InvalidReading,
-}
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, FromLeStream, ToLeStream)]
+pub struct Temperature(i16);
 
-impl From<i16> for Temperature {
-    fn from(value: i16) -> Self {
-        if value == INVALID_READING {
-            Self::InvalidReading
-        } else if RANGE.contains(&value) {
-            Self::Valid(value)
+impl Temperature {
+    /// Returns the temperature value in degrees Celsius.
+    ///
+    /// # Returns
+    ///
+    /// - `Ok(Some(value))` if the temperature is valid and within range.
+    /// - `Ok(None)` if the temperature is invalid (i.e., `-1`).
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err(value)` if the temperature is out of the valid range (-200 to 200 degrees Celsius).
+    pub fn degrees_celsius(self) -> Result<Option<i16>, i16> {
+        if self.0 == INVALID_READING {
+            Ok(None)
+        } else if RANGE.contains(&self.0) {
+            Ok(Some(self.0))
         } else {
-            Self::OutOfRange(value)
-        }
-    }
-}
-
-impl From<Temperature> for i16 {
-    fn from(temp: Temperature) -> Self {
-        match temp {
-            Temperature::OutOfRange(value) | Temperature::Valid(value) => value,
-            Temperature::InvalidReading => INVALID_READING,
-        }
-    }
-}
-
-impl FromLeStream for Temperature {
-    fn from_le_stream<T>(bytes: T) -> Option<Self>
-    where
-        T: Iterator<Item = u8>,
-    {
-        i16::from_le_stream(bytes).map(Self::from)
-    }
-}
-
-impl ToLeStream for Temperature {
-    type Iter = <i16 as ToLeStream>::Iter;
-
-    fn to_le_stream(self) -> Self::Iter {
-        match self {
-            Self::Valid(temp) | Self::OutOfRange(temp) => temp.to_le_stream(),
-            Self::InvalidReading => INVALID_READING.to_le_stream(),
+            Err(self.0)
         }
     }
 }
