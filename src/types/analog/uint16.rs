0b388593876aa5b2cc288e2e802aa108affb81e1
Richard Neumann <neumann@paulmann.de>
2025-07-29 16:03:07 +0200

    Make MAX_VALUE an associated constant.

--- src/types/analog/uint16.rs
+++ src/types/analog/uint16.rs
@@ -1,8 +1,5 @@
 use le_stream::derive::{FromLeStream, ToLeStream};
 
-/// See Table 2-11.
-const NON_VALUE: u16 = 0xffff;
-
 /// The `16-bit unsigned integer` type, short `uint16`.
 #[derive(
     Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, FromLeStream, ToLeStream,
@@ -11,10 +8,13 @@ const NON_VALUE: u16 = 0xffff;
 pub struct Uint16(u16);
 
 impl Uint16 {
+    /// The non-value. See Table 2-11.
+    pub const NON_VALUE: u16 = 0xffff;
+
     /// Crate a new `Uint16` from an `u16` value.
     #[must_use]
     pub const fn new(value: u16) -> Option<Self> {
-        if value == NON_VALUE {
+        if value == Self::NON_VALUE {
             None
         } else {
             Some(Self(value))
@@ -24,7 +24,7 @@ impl Uint16 {
 
 impl From<Uint16> for Option<u16> {
     fn from(value: Uint16) -> Self {
-        if value.0 == NON_VALUE {
+        if value.0 == Uint16::NON_VALUE {
             None
         } else {
             Some(value.0)
--- src/types/analog/uint8.rs
+++ src/types/analog/uint8.rs
@@ -1,8 +1,5 @@
 use le_stream::derive::{FromLeStream, ToLeStream};
 
-/// See Table 2-11.
-const NON_VALUE: u8 = 0xff;
-
 /// The `8-bit unsigned integer` type, short `uint8`.
 #[derive(
     Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, FromLeStream, ToLeStream,
@@ -11,10 +8,13 @@ const NON_VALUE: u8 = 0xff;
 pub struct Uint8(u8);
 
 impl Uint8 {
+    /// The non-value. See Table 2-11.
+    pub const NON_VALUE: u8 = 0xff;
+
     /// Crate a new `Uint8` from an `u8` value.
     #[must_use]
     pub const fn new(value: u8) -> Option<Self> {
-        if value == NON_VALUE {
+        if value == Self::NON_VALUE {
             None
         } else {
             Some(Self(value))
@@ -24,7 +24,7 @@ impl Uint8 {
 
 impl From<Uint8> for Option<u8> {
     fn from(value: Uint8) -> Self {
-        if value.0 == NON_VALUE {
+        if value.0 == Uint8::NON_VALUE {
             None
         } else {
             Some(value.0)
