4967794fba5ef91918cf7bab24de31c4f8d85ab1
Richard Neumann <neumann@paulmann.de>
2025-07-29 20:50:33 +0200

    Add further primitive types.

--- src/zcl/lighting/color_control/color_information_attribute.rs
+++ src/zcl/lighting/color_control/color_information_attribute.rs
@@ -4,6 +4,7 @@ use maybe_drift_compensation::MaybeDriftCompensation;
 use maybe_enhanced_color_mode::MaybeEnhancedColorMode;
 
 use super::options::Options;
+use crate::types::{String, Uint8, Uint16};
 
 mod maybe_color_mode;
 mod maybe_drift_compensation;
@@ -12,36 +13,36 @@ mod maybe_enhanced_color_mode;
 /// Color information attribute for the Color Control cluster.
 ///
 /// TODO: Add respective associated data.
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 #[repr(u16)]
 #[derive(FromLeStreamTagged)]
 pub enum ColorInformationAttribute {
     /// The current hue of the light.
-    CurrentHue = 0x0000,
+    CurrentHue(Uint8) = 0x0000,
     /// The current saturation of the light.
-    CurrentSaturation = 0x0001,
+    CurrentSaturation(Uint8) = 0x0001,
     /// Remaining time of the current command in 1/10 seconds.
-    RemainingTime = 0x0002,
+    RemainingTime(Uint16) = 0x0002,
     /// The current X coordinate in the CIE 1931 color space.
-    CurrentX = 0x0003,
+    CurrentX(Uint16) = 0x0003,
     /// The current Y coordinate in the CIE 1931 color space.
-    CurrentY = 0x0004,
+    CurrentY(Uint16) = 0x0004,
     /// The drift compensation value for the light.
     DriftCompensation(MaybeDriftCompensation) = 0x0005,
     /// The drift compensation text for the light.
-    CompensationText = 0x0006,
+    CompensationText(String) = 0x0006,
     /// The color temperature of the light in mireds.
-    ColorTemperature = 0x0007,
+    ColorTemperature(Uint16) = 0x0007,
     /// The color mode of the light.
     ColorMode(MaybeColorMode) = 0x0008,
     /// Commissioning options.
     Options(Options) = 0x000f,
     /// The enhanced current hue of the light.
-    EnhancedCurrentHue = 0x4000,
+    EnhancedCurrentHue(Uint16) = 0x4000,
     /// The enhanced color mode of the light.
     EnhancedColorMode(MaybeEnhancedColorMode) = 0x4001,
     /// Indicates whether the color loop is active.
-    ColorLoopActive = 0x4002,
+    ColorLoopActive(Uint8) = 0x4002,
     /// The direction of the color loop.
     ColorLoopDirection = 0x4003,
     /// The time for one complete color loop cycle in seconds.
