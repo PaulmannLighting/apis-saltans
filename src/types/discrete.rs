d9054a162248d6053318be5da8569cadddbfa27f
Richard Neumann <neumann@paulmann.de>
2025-07-29 12:49:23 +0200

    Implement Data40

--- src/types/discrete.rs
+++ src/types/discrete.rs
@@ -5,9 +5,11 @@ pub use data8::Data8;
 pub use data16::Data16;
 pub use data24::Data24;
 pub use data32::Data32;
+pub use data40::Data40;
 
 mod bool;
 mod data16;
 mod data24;
 mod data32;
+mod data40;
 mod data8;
---
+++ src/types/discrete/data40.rs
@@ -0,0 +1,32 @@
+use le_stream::derive::{FromLeStream, ToLeStream};
+
+/// The `40-bit data` type, short `data40`.
+#[derive(
+    Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, FromLeStream, ToLeStream,
+)]
+#[repr(transparent)]
+pub struct Data40([u8; 5]);
+
+impl AsRef<[u8]> for Data40 {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl AsMut<[u8]> for Data40 {
+    fn as_mut(&mut self) -> &mut [u8] {
+        &mut self.0
+    }
+}
+
+impl From<[u8; 5]> for Data40 {
+    fn from(value: [u8; 5]) -> Self {
+        Self(value)
+    }
+}
+
+impl From<Data40> for [u8; 5] {
+    fn from(value: Data40) -> Self {
+        value.0
+    }
+}
