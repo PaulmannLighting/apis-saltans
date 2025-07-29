83f657eec92c12a274b0674c5b0369d87ec303eb
Richard Neumann <neumann@paulmann.de>
2025-07-29 11:38:53 +0200

    Implement Data32

--- src/types/discrete.rs
+++ src/types/discrete.rs
@@ -3,7 +3,9 @@
 pub use data8::Data8;
 pub use data16::Data16;
 pub use data24::Data24;
+pub use data32::Data32;
 
 mod data16;
 mod data24;
+mod data32;
 mod data8;
---
+++ src/types/discrete/data32.rs
@@ -0,0 +1,32 @@
+use le_stream::derive::{FromLeStream, ToLeStream};
+
+/// The `32-bit data` type, short `data32`.
+#[derive(
+    Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, FromLeStream, ToLeStream,
+)]
+#[repr(transparent)]
+pub struct Data32([u8; 4]);
+
+impl AsRef<[u8]> for Data32 {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl AsMut<[u8]> for Data32 {
+    fn as_mut(&mut self) -> &mut [u8] {
+        &mut self.0
+    }
+}
+
+impl From<[u8; 4]> for Data32 {
+    fn from(value: [u8; 4]) -> Self {
+        Self(value)
+    }
+}
+
+impl From<Data32> for [u8; 4] {
+    fn from(value: Data32) -> Self {
+        value.0
+    }
+}
