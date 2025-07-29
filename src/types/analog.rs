1e2c3f9ee964576fa39fa707a8d03fa4be4d08fe
Richard Neumann <neumann@paulmann.de>
2025-07-29 13:00:51 +0200

    Implement Uint16.

--- src/types/analog.rs
+++ src/types/analog.rs
@@ -1,5 +1,7 @@
 //! Analog data types.
 
 pub use uint8::Uint8;
+pub use uint16::Uint16;
 
+mod uint16;
 mod uint8;
---
+++ src/types/analog/uint16.rs
@@ -0,0 +1,27 @@
+use le_stream::derive::{FromLeStream, ToLeStream};
+
+/// See Table 2-11.
+const NON_VALUE: u16 = 0xffff;
+
+/// The `16-bit unsigned integer` type, short `uint16`.
+#[derive(
+    Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, FromLeStream, ToLeStream,
+)]
+#[repr(transparent)]
+pub struct Uint16(u16);
+
+impl From<u16> for Uint16 {
+    fn from(value: u16) -> Self {
+        Self(value)
+    }
+}
+
+impl From<Uint16> for Option<u16> {
+    fn from(value: Uint16) -> Self {
+        if value.0 == NON_VALUE {
+            None
+        } else {
+            Some(value.0)
+        }
+    }
+}
