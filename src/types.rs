150fdf4b6467e86a87a628245be33d68c96fb0b8
Richard Neumann <neumann@paulmann.de>
2025-07-29 15:17:21 +0200

    Refactor composite submodule.

--- src/types.rs
+++ src/types.rs
@@ -4,10 +4,7 @@ pub use analog::{
     Int8, Int16, Int24, Int32, Int40, Int48, Int56, Int64, Uint8, Uint16, Uint24, Uint32, Uint40,
     Uint48, Uint56, Uint64,
 };
-pub use composite::oct_str::OctStr;
-pub use composite::oct_str16::OctStr16;
-pub use composite::string::String;
-pub use composite::string16::String16;
+pub use composite::{OctStr, OctStr16, String, String16};
 pub use discrete::{Bool, Data8, Data16, Data24, Data32, Data40, Data48, Data56, Data64};
 pub use null::{NoData, Unknown};
 
--- src/types/composite.rs
+++ src/types/composite.rs
@@ -1,4 +1,11 @@
-pub mod oct_str;
-pub mod oct_str16;
-pub mod string;
-pub mod string16;
+//! Composite data types.
+
+pub use oct_str::OctStr;
+pub use oct_str16::OctStr16;
+pub use string::String;
+pub use string16::String16;
+
+mod oct_str;
+mod oct_str16;
+mod string;
+mod string16;
