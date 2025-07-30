9787f9f3631c15e1069b7d75e2761766883bc653
Richard Neumann <neumann@paulmann.de>
2025-07-30 12:40:04 +0200

    Use internal data types.

--- src/zcl/device_temperature_configuration/attribute.rs
+++ src/zcl/device_temperature_configuration/attribute.rs
@@ -1,12 +1,12 @@
 use core::iter::Chain;
 
-use intx::U24;
 use le_stream::ToLeStream;
 use le_stream::derive::FromLeStreamTagged;
 use repr_discriminant::ReprDiscriminant;
 
 use super::temp_threshold::TempThreshold;
 use super::{DeviceTempAlarmMask, Temperature};
+use crate::types::{Uint16, Uint24};
 
 /// Attributes for the Device Temperature Configuration cluster.
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -21,7 +21,7 @@ pub enum Attribute {
     /// Maximum temperature experienced in degrees Celsius.
     MaxTempExperienced(Temperature) = 0x0002,
     /// Total time the temperature was above the maximum threshold in hours.
-    OverTempTotalDwell(u16) = 0x0003,
+    OverTempTotalDwell(Uint16) = 0x0003,
     // Device Temperature Settings.
     /// Alarms mask for device temperature.
     DeviceTempAlarmMask(DeviceTempAlarmMask) = 0x0010,
@@ -30,9 +30,9 @@ pub enum Attribute {
     /// High temperature threshold in degrees Celsius.
     HighTempThreshold(TempThreshold) = 0x0012,
     /// Low temperature dwell trip point in seconds.
-    LowTempDwellTripPoint(U24) = 0x0013,
+    LowTempDwellTripPoint(Uint24) = 0x0013,
     /// High temperature dwell trip point in seconds.
-    HighTempDwellTripPoint(U24) = 0x0014,
+    HighTempDwellTripPoint(Uint24) = 0x0014,
 }
 
 impl ToLeStream for Attribute {
@@ -57,19 +57,19 @@ impl ToLeStream for Attribute {
 
 /// Iterator for `Attribute` payloads.
 mod iterator {
-    use intx::U24;
     use le_stream::ToLeStream;
 
+    use crate::types::{Uint16, Uint24};
     use crate::zcl::device_temperature_configuration::{
         DeviceTempAlarmMask, TempThreshold, Temperature,
     };
 
     pub enum Attribute {
         Temperature(<Temperature as ToLeStream>::Iter),
-        U16(<u16 as ToLeStream>::Iter),
+        Uint16(<Uint16 as ToLeStream>::Iter),
         DeviceTempAlarmMask(<DeviceTempAlarmMask as ToLeStream>::Iter),
         TempThreshold(<TempThreshold as ToLeStream>::Iter),
-        U24(<U24 as ToLeStream>::Iter),
+        Uint24(<Uint24 as ToLeStream>::Iter),
     }
 
     impl Iterator for Attribute {
@@ -77,11 +77,11 @@ mod iterator {
 
         fn next(&mut self) -> Option<Self::Item> {
             match self {
-                Self::U16(iter) | Self::Temperature(iter) | Self::TempThreshold(iter) => {
+                Self::Uint16(iter) | Self::Temperature(iter) | Self::TempThreshold(iter) => {
                     iter.next()
                 }
                 Self::DeviceTempAlarmMask(iter) => iter.next(),
-                Self::U24(iter) => iter.next(),
+                Self::Uint24(iter) => iter.next(),
             }
         }
     }
@@ -92,9 +92,9 @@ mod iterator {
         }
     }
 
-    impl From<u16> for Attribute {
-        fn from(value: u16) -> Self {
-            Self::U16(value.to_le_stream())
+    impl From<Uint16> for Attribute {
+        fn from(value: Uint16) -> Self {
+            Self::Uint16(value.to_le_stream())
         }
     }
 
@@ -110,9 +110,9 @@ mod iterator {
         }
     }
 
-    impl From<U24> for Attribute {
-        fn from(value: U24) -> Self {
-            Self::U24(value.to_le_stream())
+    impl From<Uint24> for Attribute {
+        fn from(value: Uint24) -> Self {
+            Self::Uint24(value.to_le_stream())
         }
     }
 }
