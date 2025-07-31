33c03852a6c92d80be16adf7556fde4a8621fced
Richard Neumann <neumann@paulmann.de>
2025-07-31 10:53:55 +0200

    Refactor DateCode.

--- src/zcl/basic/attribute/date_code.rs
+++ src/zcl/basic/attribute/date_code.rs
@@ -1,22 +1,17 @@
-use alloc::string::{String, ToString};
+use alloc::string::ToString;
 use core::fmt::Display;
 use core::str::FromStr;
 
 use chrono::NaiveDate;
-use le_stream::{FromLeStream, ToLeStream};
 pub use parse_error::ParseError;
+
+use crate::types::String;
 
 mod parse_error;
 
 const DATE_FORMAT: &str = "%Y%m%d";
-const DATE_SIZE: usize = 8;
 const MAX_CUSTOM_SIZE: usize = 8;
-const MAX_SIZE: usize = DATE_SIZE + MAX_CUSTOM_SIZE;
 
-/// Zigbee Date Code string type, which is a fixed-size string of 16 bytes.
-pub type DateCodeString = heapless::String<MAX_SIZE>;
-/// A vector of bytes representing the date code, which can be up to `MAX_SIZE` bytes long.
-pub type DateCodeBytes = heapless::Vec<u8, MAX_SIZE>;
 /// A custom string type for the custom part of the date code, which can be up to `MAX_CUSTOM_SIZE` bytes long.
 pub type CustomString = heapless::String<MAX_CUSTOM_SIZE>;
 
@@ -50,26 +45,22 @@ impl DateCode {
 
 impl Display for DateCode {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        DateCodeString::from(self).fmt(f)
+        write!(f, "{}{}", self.date.format(DATE_FORMAT), self.custom)
     }
 }
 
-impl From<DateCode> for DateCodeString {
+impl From<DateCode> for String {
     fn from(date_code: DateCode) -> Self {
         Self::from(&date_code)
     }
 }
 
-impl From<&DateCode> for DateCodeString {
+impl From<&DateCode> for String {
     fn from(date_code: &DateCode) -> Self {
-        let mut string = Self::new();
-        string
-            .push_str(&date_code.date.format(DATE_FORMAT).to_string())
-            .expect("Serialized date should fit into DateCodeString.");
-        string
-            .push_str(date_code.custom())
-            .expect("Custom part should fit into DateCodeString.");
-        string
+        date_code
+            .to_string()
+            .try_into()
+            .expect("Date code should fit into a String.")
     }
 }
 
@@ -81,24 +72,5 @@ impl FromStr for DateCode {
         let mut custom = CustomString::new();
         custom.push_str(remainder)?;
         Ok(Self::new(date, custom))
-    }
-}
-
-impl FromLeStream for DateCode {
-    fn from_le_stream<T>(bytes: T) -> Option<Self>
-    where
-        T: Iterator<Item = u8>,
-    {
-        String::from_utf8_lossy(&bytes.take(MAX_SIZE).collect::<DateCodeBytes>())
-            .parse()
-            .ok()
-    }
-}
-
-impl ToLeStream for DateCode {
-    type Iter = <DateCodeBytes as IntoIterator>::IntoIter;
-
-    fn to_le_stream(self) -> Self::Iter {
-        DateCodeString::from(self).into_bytes().into_iter()
     }
 }
