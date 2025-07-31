74e6fe4b62045843688ab9edaaf3584d88777394
Richard Neumann <neumann@paulmann.de>
2025-07-31 09:58:31 +0200

    Implement ViewGroupResponse.

--- src/zcl/groups/commands/view_group_response.rs
+++ src/zcl/groups/commands/view_group_response.rs
@@ -1,3 +1,5 @@
+use core::str::Utf8Error;
+
 use le_stream::derive::{FromLeStream, ToLeStream};
 
 use crate::types::{String, Uint16};
@@ -8,9 +10,9 @@ use crate::zcl::{Cluster, Command, Status};
 /// Represents a response to an `AddGroups` command.
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, FromLeStream, ToLeStream)]
 pub struct ViewGroupResponse {
-    pub status: u8,
-    pub group_id: Uint16,
-    pub group_name: String,
+    status: u8,
+    group_id: Uint16,
+    group_name: String,
 }
 
 impl ViewGroupResponse {
@@ -37,6 +39,15 @@ impl ViewGroupResponse {
     #[must_use]
     pub const fn group_id(&self) -> Uint16 {
         self.group_id
+    }
+
+    /// Returns the name of the group associated with the response.
+    ///
+    /// # Errors
+    ///
+    /// If the group name is not valid UTF-8, this will return an [`Utf8Error`].
+    pub fn group_name(&self) -> Result<&str, Utf8Error> {
+        self.group_name.try_as_str()
     }
 }
 
