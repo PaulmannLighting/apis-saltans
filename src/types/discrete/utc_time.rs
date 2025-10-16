e1e985f50d43407f240efbd7fb26d531711ed707
Richard Neumann <neumann@paulmann.de>
2025-10-16 14:19:29 +0200

    Refactor UTC time conversion.

--- src/types/discrete/utc_time.rs
+++ src/types/discrete/utc_time.rs
@@ -11,54 +11,62 @@ const BASE_DATETIME: DateTime<Utc> = DateTime::<Utc>::from_naive_utc_and_offset(
     ),
     Utc,
 );
+const NON_VALUE: u32 = 0xffff_ffff;
 
 /// UTC time data type.
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct UtcTime(u32);
 
-impl UtcTime {
-    /// Creates a new `UtcTime` instance from a [`TimeDelta`].
-    ///
-    /// # Returns
-    ///
-    /// A tuple of the `UtcTime` instance and the remaining nanoseconds.
-    ///
-    /// # Errors
-    ///
-    /// Returns an [`TryFromIntError`] if the seconds part of the `TimeDelta` cannot be converted to `u32`.
-    pub fn try_from_time_delta(time_delta: TimeDelta) -> Result<(Self, i32), TryFromIntError> {
-        time_delta
-            .num_seconds()
-            .try_into()
-            .map(|seconds| (Self(seconds), time_delta.subsec_nanos()))
-    }
-
-    /// Creates a new `UtcTime` instance from a [`DateTime`].
-    ///
-    /// # Returns
-    ///
-    /// A tuple of the `UtcTime` instance and the remaining nanoseconds.
-    ///
-    /// # Errors
-    ///
-    /// Returns an [`TryFromIntError`] if the seconds part of the `TimeDelta` cannot be converted to `u32`.
-    pub fn try_from_date_time<T>(date_time: DateTime<T>) -> Result<(Self, i32), TryFromIntError>
-    where
-        T: TimeZone,
-    {
-        Self::try_from_time_delta(date_time.signed_duration_since(BASE_DATETIME))
+impl From<UtcTime> for Option<u32> {
+    fn from(value: UtcTime) -> Self {
+        if value.0 == NON_VALUE {
+            None
+        } else {
+            Some(value.0)
+        }
     }
 }
 
-impl From<UtcTime> for TimeDelta {
-    fn from(value: UtcTime) -> Self {
-        Self::seconds(i64::from(value.0))
+impl TryFrom<TimeDelta> for UtcTime {
+    type Error = TryFromIntError;
+
+    /// Converts a `TimeDelta` to a `UtcTime`.
+    ///
+    /// This will discard any sub-second information.
+    fn try_from(value: TimeDelta) -> Result<Self, Self::Error> {
+        value.num_seconds().try_into().map(Self)
     }
 }
 
-impl From<UtcTime> for DateTime<Utc> {
-    fn from(value: UtcTime) -> Self {
-        BASE_DATETIME.add(TimeDelta::from(value))
+impl<T> TryFrom<DateTime<T>> for UtcTime
+where
+    T: TimeZone,
+{
+    type Error = TryFromIntError;
+
+    /// Converts a `DateTime` to a `UtcTime`.
+    ///
+    /// This will discard any sub-second information.
+    fn try_from(value: DateTime<T>) -> Result<Self, Self::Error> {
+        Self::try_from(value.signed_duration_since(BASE_DATETIME))
+    }
+}
+
+impl TryFrom<UtcTime> for TimeDelta {
+    type Error = ();
+
+    fn try_from(value: UtcTime) -> Result<Self, Self::Error> {
+        Option::<u32>::from(value)
+            .ok_or(())
+            .map(|seconds| Self::seconds(seconds.into()))
+    }
+}
+
+impl TryFrom<UtcTime> for DateTime<Utc> {
+    type Error = ();
+
+    fn try_from(value: UtcTime) -> Result<Self, Self::Error> {
+        TimeDelta::try_from(value).map(|time_delta| BASE_DATETIME.add(time_delta))
     }
 }
 
@@ -69,22 +77,20 @@ mod tests {
     #[test]
     fn datetime_from_utc_no_offset() {
         let utc_time = UtcTime(0);
-        let datetime: DateTime<Utc> = utc_time.into();
+        let datetime: DateTime<Utc> = utc_time.try_into().unwrap();
         assert_eq!(datetime, BASE_DATETIME);
     }
 
     #[test]
     fn datetime_from_utc_with_offset() {
         let utc_time = UtcTime(1000);
-        let datetime: DateTime<Utc> = utc_time.into();
+        let datetime: DateTime<Utc> = utc_time.try_into().unwrap();
         assert_eq!(datetime, BASE_DATETIME.add(TimeDelta::seconds(1000)));
     }
 
     #[test]
     fn try_from_utc_time() {
-        let (utc_time, nanos) =
-            UtcTime::try_from_date_time(BASE_DATETIME).expect("Failed to convert datetime.");
+        let utc_time: UtcTime = BASE_DATETIME.try_into().unwrap();
         assert_eq!(utc_time, UtcTime(0));
-        assert_eq!(nanos, 0);
     }
 }
