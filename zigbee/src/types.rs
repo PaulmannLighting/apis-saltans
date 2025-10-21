83f3c0e6807a90a57086ec75b3e5f6a41c030e30
Richard Neumann <neumann@paulmann.de>
2025-10-21 10:55:39 +0200

    Remove `List` type.

--- zigbee/src/types.rs
+++ zigbee/src/types.rs
@@ -4,7 +4,7 @@ pub use analog::{
     Int8, Int16, Int24, Int32, Int40, Int48, Int56, Int64, Uint8, Uint16, Uint24, Uint32, Uint40,
     Uint48, Uint56, Uint64,
 };
-pub use composite::{List, OctStr, String};
+pub use composite::{OctStr, String};
 pub use discrete::{
     Bool, Data8, Data16, Data24, Data32, Data40, Data48, Data56, Data64, Date, TimeOfDay,
     TryFromNaiveDateError, TryFromNaiveTimeError, TryIntoNaiveDateError, TryIntoNaiveTimeError,
