c40652e7a847e5f271f15b168b23affa375a436f
Richard Neumann <neumann@paulmann.de>
2025-11-12 08:34:35 +0100

    Export `BallastSettingsAttribute`.

--- zcl/src/clusters/lighting/ballast_configuration.rs
+++ zcl/src/clusters/lighting/ballast_configuration.rs
@@ -2,6 +2,7 @@
 
 pub use self::ballast_configuration_attribute::BallastConfigurationAttribute;
 pub use self::ballast_information_attribute::BallastInformationAttribute;
+pub use self::ballast_settings_attribute::BallastSettingsAttribute;
 pub use self::ballast_status::BallastStatus;
 
 mod ballast_configuration_attribute;
