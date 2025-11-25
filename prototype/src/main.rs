682c77b484bf752970ef8c78bfea870f920966f7
Richard Neumann <neumann@paulmann.de>
2025-11-25 10:32:23 +0100

    Implement prototype.

---
+++ prototype/src/main.rs
@@ -0,0 +1,25 @@
+//! A prototype for testing the EZSP UART implementation.
+
+use ashv2::{BaudRate, open};
+use clap::Parser;
+use ezsp::uart::Uart;
+use prototyping::Coordinator;
+use serialport::FlowControl;
+
+#[derive(Debug, Parser)]
+struct Args {
+    tty: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let serial_port = open(args.tty.clone(), BaudRate::RstCts, FlowControl::Software)
+        .expect("Failed to open serial port");
+    let (callbacks_sender, _callbacks_receiver) = tokio::sync::mpsc::channel(1024);
+    let mut uart = Uart::new(serial_port, callbacks_sender, 8, 1024);
+    uart.initialize().await.expect("Failed to initialize uart");
+    uart.form_network(0xabcd, 11)
+        .await
+        .expect("Failed to form network");
+}
