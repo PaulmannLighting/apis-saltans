41ff7a3c151644d3fec056e228718fc3dc8c4d5c
Richard Neumann <neumann@paulmann.de>
2025-12-02 17:07:31 +0100

    [WIP] Working on APS unicasts.

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,26 +1,35 @@
 //! A prototype for testing the EZSP UART implementation.
 
 use std::collections::BTreeMap;
+use std::io::stdin;
 use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 use std::time::Duration;
 
 use ashv2::{BaudRate, open};
 use clap::Parser;
-use ezsp::Ezsp;
-use ezsp::ember::concentrator;
+use ezsp::ember::message::Destination;
+use ezsp::ember::{aps, concentrator};
 use ezsp::ezsp::{config, decision, policy};
 use ezsp::uart::Uart;
 use ezsp::zigbee::{DeviceConfig, EventHandler, NetworkManager};
+use ezsp::{Ezsp, Messaging, Networking};
+use le_stream::ToLeStream;
 use log::{debug, info};
 use macaddr::MacAddr8;
 use serialport::FlowControl;
+use tokio::time::sleep;
+use zcl::Cluster;
+use zcl::general::identify::{EffectIdentifier, EffectVariant, TriggerEffect};
+use zcl::general::on_off::Off;
+use zcl::lighting::color_control::MoveToColor;
 use zigbee_nwk::Nlme;
 
 const PAN_ID: u16 = 24171;
 const EXTENDED_PAN_ID: MacAddr8 = MacAddr8::new(0x8D, 0x9F, 0x3D, 0xFE, 0x00, 0xBF, 0x0D, 0xB5);
 const RADIO_CHANNEL: u8 = 11;
 const LINK_KEY: &[u8] = include_bytes!("../../assets/link.key");
+const HOME_AUTOMATION: u16 = 0x0104;
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -37,9 +46,11 @@ struct Args {
     reinitialize: bool,
     #[clap(long, short, help = "The PAN ID for the new network", default_value_t = PAN_ID)]
     pan_id: u16,
-    #[clap(long, short, help = "The Extended PAN ID for the new network", default_value_t = EXTENDED_PAN_ID)]
+    #[clap(long, short, help = "The Extended PAN ID for the new network", default_value_t = EXTENDED_PAN_ID
+    )]
     extended_pan_id: MacAddr8,
-    #[clap(long, short = 'c', help = "The radio channel for the new network", default_value_t = RADIO_CHANNEL)]
+    #[clap(long, short = 'c', help = "The radio channel for the new network", default_value_t = RADIO_CHANNEL
+    )]
     radio_channel: u8,
 }
 
@@ -69,34 +80,30 @@ async fn main() {
     .expect("Concentrator parameters should be valid.");
 
     let mut configuration = BTreeMap::new();
-    configuration.insert(config::Id::IndirectTransmissionTimeout, 7680);
-    configuration.insert(config::Id::MaxHops, 30);
-    configuration.insert(config::Id::PacketBufferCount, 250);
-    configuration.insert(config::Id::FragmentDelayMs, 50);
+    configuration.insert(config::Id::SourceRouteTableSize, 16);
+    configuration.insert(config::Id::SecurityLevel, 5);
     configuration.insert(config::Id::AddressTableSize, 8);
-    configuration.insert(config::Id::TxPowerMode, 0);
-    configuration.insert(config::Id::ApsUnicastMessageCount, 16);
-    configuration.insert(config::Id::SupportedNetworks, 1);
-    configuration.insert(config::Id::ApplicationZdoFlags, 1);
     configuration.insert(config::Id::TrustCenterAddressCacheSize, 2);
     configuration.insert(config::Id::StackProfile, 2);
-    configuration.insert(config::Id::BroadcastTableSize, 15);
-    configuration.insert(config::Id::NeighborTableSize, 24);
-    configuration.insert(config::Id::MaxEndDeviceChildren, 16);
-    configuration.insert(config::Id::SourceRouteTableSize, 100);
-    configuration.insert(config::Id::SecurityLevel, 5);
+    configuration.insert(config::Id::IndirectTransmissionTimeout, 7680);
+    configuration.insert(config::Id::MaxHops, 8);
+    configuration.insert(config::Id::TxPowerMode, 0);
+    configuration.insert(config::Id::SupportedNetworks, 1);
     configuration.insert(config::Id::KeyTableSize, 4);
-    configuration.insert(config::Id::FragmentWindowSize, 1);
+    configuration.insert(config::Id::ApplicationZdoFlags, 1);
+    configuration.insert(config::Id::MaxEndDeviceChildren, 16);
+    configuration.insert(config::Id::ApsUnicastMessageCount, 10);
+    configuration.insert(config::Id::BroadcastTableSize, 15);
     configuration.insert(config::Id::BindingTableSize, 2);
+    configuration.insert(config::Id::NeighborTableSize, 16);
+    configuration.insert(config::Id::FragmentWindowSize, 1);
+    configuration.insert(config::Id::FragmentDelayMs, 50);
+    configuration.insert(config::Id::PacketBufferCount, 255);
 
     let mut policy = BTreeMap::new();
     policy.insert(
         policy::Id::TcKeyRequest,
         decision::Id::AllowTcKeyRequestsAndSendCurrentKey,
-    );
-    policy.insert(
-        policy::Id::MessageContentsInCallback,
-        decision::Id::MessageTagOnlyInCallback,
     );
     policy.insert(
         policy::Id::TrustCenter,
@@ -106,6 +113,11 @@ async fn main() {
         policy::Id::TcJoinsUsingWellKnownKey,
         decision::Id::AllowJoins,
     );
+    policy.insert(
+        policy::Id::MessageContentsInCallback,
+        decision::Id::MessageTagAndContentsInCallback,
+    );
+    policy.insert(policy::Id::KeyRequest, decision::Id::DenyAppKeyRequests);
     policy.insert(
         policy::Id::BindingModification,
         decision::Id::CheckBindingModificationsAreValidEndpointClusters,
@@ -157,6 +169,228 @@ async fn main() {
         .await
         .expect("Failed to allow joins");
 
+    info!("Waiting for network to open...");
     network_manager.await_network_open().await;
+    info!("Network is open.");
+
+    info!("Waiting for network to close...");
     network_manager.await_network_closed().await;
+    info!("Joining period has ended.");
+
+    network_manager
+        .send_many_to_one_route_request()
+        .await
+        .expect("Failed to send many route request");
+    sleep(Duration::from_secs(1)).await;
+
+    info!("Sending active endpoint request...");
+    active_endpoint_request(&mut network_manager, 200).await;
+
+    /*
+    sleep(Duration::from_secs(1)).await;
+
+    let mut sequence = Sequence(0x00);
+
+    info!("Switching off all connected devices...");
+    switch_off(&mut network_manager, sequence.next()).await;
+
+    sleep(Duration::from_secs(1)).await;
+
+    info!("Setting color on all connected devices...");
+    move_to_color(&mut network_manager, sequence.next()).await;
+
+    sleep(Duration::from_secs(1)).await;
+
+    info!("Triggering effect on all connected devices...");
+    trigger_effect(&mut network_manager, sequence.next()).await;
+    */
+
+    info!("Waiting 10 seconds...");
+    sleep(Duration::from_secs(10)).await;
+
+    info!("Sending many-to-one route request.");
+    network_manager
+        .send_many_to_one_route_request()
+        .await
+        .expect("Failed to send many-to-one route request");
+
+    info!("Waiting 10 seconds...");
+    sleep(Duration::from_secs(10)).await;
+
+    for neighbor in network_manager
+        .get_neighbors()
+        .await
+        .expect("Failed to get neighbors")
+    {
+        info!("Neighbor: {neighbor:?}");
+    }
+
+    for child in network_manager
+        .get_children()
+        .await
+        .expect("Failed to get children")
+    {
+        info!("Child: {child:?}");
+    }
+
+    info!("Sending active endpoint request...");
+    active_endpoint_request(&mut network_manager, 201).await;
+
+    let _ = stdin().lines().next();
+}
+
+async fn move_to_color<T>(network_manager: &mut NetworkManager<T>, sequence: u8)
+where
+    T: Messaging + Networking,
+{
+    let move_to_color = MoveToColor::new(0x529E, 0x543B, 0, 0x00, 0x00);
+    let aps_options = aps::Options::RETRY
+        | aps::Options::ENABLE_ROUTE_DISCOVERY
+        | aps::Options::ENABLE_ADDRESS_DISCOVERY;
+    let aps_frame = aps::Frame::new(
+        HOME_AUTOMATION,
+        <MoveToColor as Cluster>::ID,
+        0x01,
+        0x01,
+        aps_options,
+        0x00,
+        sequence,
+    );
+    let zcl_frame = zcl::Frame::new(
+        zcl::Type::ClusterSpecific,
+        zcl::Direction::ClientToServer,
+        false,
+        None,
+        sequence,
+        move_to_color,
+    );
+
+    send_to_all(network_manager, aps_frame, zcl_frame).await;
+}
+
+async fn trigger_effect<T>(network_manager: &mut NetworkManager<T>, sequence: u8)
+where
+    T: Messaging + Networking,
+{
+    let trigger_effect = TriggerEffect::new(EffectIdentifier::Blink, EffectVariant::Default);
+    let aps_options = aps::Options::RETRY
+        | aps::Options::ENABLE_ROUTE_DISCOVERY
+        | aps::Options::ENABLE_ADDRESS_DISCOVERY;
+    let aps_frame = aps::Frame::new(
+        HOME_AUTOMATION,
+        <TriggerEffect as Cluster>::ID,
+        0x01,
+        0x01,
+        aps_options,
+        0x00,
+        sequence,
+    );
+    let zcl_frame = zcl::Frame::new(
+        zcl::Type::ClusterSpecific,
+        zcl::Direction::ClientToServer,
+        false,
+        None,
+        sequence,
+        trigger_effect,
+    );
+
+    send_to_all(network_manager, aps_frame, zcl_frame).await;
+}
+
+async fn switch_off<T>(network_manager: &mut NetworkManager<T>, sequence: u8)
+where
+    T: Messaging + Networking,
+{
+    let aps_options = aps::Options::RETRY
+        | aps::Options::ENABLE_ROUTE_DISCOVERY
+        | aps::Options::ENABLE_ADDRESS_DISCOVERY;
+    let aps_frame = aps::Frame::new(
+        HOME_AUTOMATION,
+        <Off as Cluster>::ID,
+        0x01,
+        0x01,
+        aps_options,
+        0x00,
+        sequence,
+    );
+    let zcl_frame = zcl::Frame::new(
+        zcl::Type::ClusterSpecific,
+        zcl::Direction::ClientToServer,
+        false,
+        None,
+        sequence,
+        Off,
+    );
+
+    send_to_all(network_manager, aps_frame, zcl_frame).await;
+}
+
+async fn active_endpoint_request<T>(network_manager: &mut NetworkManager<T>, sequence: u8)
+where
+    T: Messaging + Networking,
+{
+    let aps_options = aps::Options::RETRY
+        | aps::Options::ENABLE_ROUTE_DISCOVERY
+        | aps::Options::ENABLE_ADDRESS_DISCOVERY;
+    let aps_frame = aps::Frame::new(0x0000, 0x0005, 0x01, 0x01, aps_options, 0x00, sequence);
+
+    let mut payload = Vec::new();
+
+    for (_, short_address) in network_manager
+        .get_neighbors()
+        .await
+        .expect("Failed to get neighbors")
+    {
+        payload.clear();
+        payload.push(0x00); // ZDP SEQ
+        payload.extend(short_address.to_le_stream()); // NWK Address
+        info!("Setting color on device with short ID {short_address}");
+        network_manager
+            .send_unicast(
+                Destination::Direct(short_address),
+                aps_frame.clone(),
+                &payload,
+            )
+            .await
+            .expect("Failed to send unicast data");
+    }
+}
+
+async fn send_to_all<N, F>(
+    network_manager: &mut NetworkManager<N>,
+    aps_frame: aps::Frame,
+    zcl_frame: zcl::Frame<F>,
+) where
+    N: Messaging + Networking,
+    F: Clone + zcl::Command + ToLeStream,
+{
+    debug!("Sending APS frame: {aps_frame:#X?}");
+    let payload = zcl_frame.to_le_stream().collect::<Vec<_>>();
+    debug!("Sending payload: {payload:#04X?}");
+
+    for (_, short_address) in network_manager
+        .get_neighbors()
+        .await
+        .expect("Failed to get neighbors")
+    {
+        info!("Setting color on device with short ID {short_address}");
+        network_manager
+            .send_unicast(
+                Destination::Direct(short_address),
+                aps_frame.clone(),
+                &payload,
+            )
+            .await
+            .expect("Failed to send unicast data");
+    }
+}
+
+struct Sequence(u8);
+
+impl Sequence {
+    pub const fn next(&mut self) -> u8 {
+        let current = self.0;
+        self.0 = self.0.wrapping_add(1);
+        current
+    }
 }
