6c28caee05a0791b0fdd054ee952edf79862b0d9
Richard Neumann <neumann@paulmann.de>
2025-12-04 15:21:05 +0100

    Add constructor to `AckFmt`.

--- aps/src/frame/acknowledgement/ack_fmt.rs
+++ aps/src/frame/acknowledgement/ack_fmt.rs
@@ -11,3 +11,40 @@ pub struct AckFmt {
     profile_id: u16,
     source: u8,
 }
+
+impl AckFmt {
+    /// Creates a new `AckFmt`.
+    #[must_use]
+    pub const fn new(destination: u8, cluster_id: u16, profile_id: u16, source: u8) -> Self {
+        Self {
+            destination,
+            cluster_id,
+            profile_id,
+            source,
+        }
+    }
+
+    /// Returns the destination endpoint.
+    #[must_use]
+    pub const fn destination(&self) -> u8 {
+        self.destination
+    }
+
+    /// Returns the cluster ID.
+    #[must_use]
+    pub const fn cluster_id(&self) -> u16 {
+        self.cluster_id
+    }
+
+    /// Returns the profile ID.
+    #[must_use]
+    pub const fn profile_id(&self) -> u16 {
+        self.profile_id
+    }
+
+    /// Returns the source endpoint.
+    #[must_use]
+    pub const fn source(&self) -> u8 {
+        self.source
+    }
+}
