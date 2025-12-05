f8509c7ecd41afb55aa4e11b50d5e93ffb8b4881
Richard Neumann <neumann@paulmann.de>
2025-12-05 23:37:36 +0100

    Revamp `ReceivedApsFrame`.

--- nwk/src/event/aps_frame.rs
+++ nwk/src/event/aps_frame.rs
@@ -1,37 +1,42 @@
-use aps::{Acknowledgment, Command, Control, Data, FrameType};
-use le_stream::FromLeStream;
-use log::error;
+use aps::{Control, Destination};
 
 /// An APS frame that has been received.
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum ReceivedApsFrame {
-    /// Acknowledgment frame.
-    Ack(Acknowledgment),
-    /// Command frame.
-    Command(Command<Box<[u8]>>),
-    /// Data frame.
-    Data(Data<Box<[u8]>>),
+pub struct ReceivedApsFrame {
+    // FIXME: E.g. EZSP does not provide the full APS frame, so we need to keep the control optional.
+    control: Option<Control>,
+    destination: Destination,
+    cluster_id: u16,
+    profile_id: u16,
+    source_endpoint: u8,
+    aps_counter: u8,
+    extended: Option<aps::Extended>,
+    payload: Box<[u8]>,
 }
 
-impl FromLeStream for ReceivedApsFrame {
-    fn from_le_stream<T>(mut bytes: T) -> Option<Self>
-    where
-        T: Iterator<Item = u8>,
-    {
-        let control = Control::from_le_stream(&mut bytes)?;
-
-        match control.frame_type() {
-            FrameType::Acknowledgment => {
-                Acknowledgment::from_le_stream_with_control(control, bytes).map(Self::Ack)
-            }
-            FrameType::Command => {
-                Command::from_le_stream_with_control(control, bytes).map(Self::Command)
-            }
-            FrameType::Data => Data::from_le_stream_with_control(control, bytes).map(Self::Data),
-            FrameType::InterPanAps => {
-                error!("Received Inter-PAN APS frame, which is not supported.");
-                None
-            }
+impl ReceivedApsFrame {
+    /// Creates a new `ReceivedApsFrame`.
+    #[expect(clippy::too_many_arguments)]
+    #[must_use]
+    pub const fn new(
+        control: Option<Control>,
+        destination: Destination,
+        cluster_id: u16,
+        profile_id: u16,
+        source_endpoint: u8,
+        aps_counter: u8,
+        extended: Option<aps::Extended>,
+        payload: Box<[u8]>,
+    ) -> Self {
+        Self {
+            control,
+            destination,
+            cluster_id,
+            profile_id,
+            source_endpoint,
+            aps_counter,
+            extended,
+            payload,
         }
     }
 }
