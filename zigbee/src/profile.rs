f6bcc47a5cfbd6aaff5bae7fcd018557fbef9a18
Richard Neumann <neumann@paulmann.de>
2025-12-09 18:28:02 +0100

    Allow conversion from `u16` for `Profile`.

--- zigbee/src/profile.rs
+++ zigbee/src/profile.rs
@@ -1,5 +1,8 @@
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
 /// Defines the Zigbee Profile Identifiers as per the Zigbee specification.
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, FromPrimitive)]
 #[repr(u16)]
 pub enum Profile {
     /// Profile Identifier for Unspecified Profile.
@@ -10,4 +13,12 @@ pub enum Profile {
     SmartEnergy = 0x0109,
     /// Profile Identifier for Light Link Profile.
     Touchlink = 0xC05E,
+}
+
+impl TryFrom<u16> for Profile {
+    type Error = u16;
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        Self::from_u16(value).ok_or(value)
+    }
 }
