b5a14fdfafbf4effc6d3b01cc63ae122bc1cb863
Richard Neumann <neumann@paulmann.de>
2025-12-09 18:57:05 +0100

    Implement `TryFrom` for `Clusters`.

--- zdp/src/clusters.rs
+++ zdp/src/clusters.rs
@@ -1,9 +1,11 @@
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
 use zigbee::Cluster;
 
 use crate::{BindReq, IeeeAddrReq, MgmtPermitJoiningReq, NwkAddrReq};
 
 /// Available ZDP clusters.
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+#[derive(Clone, Debug, Eq, PartialEq, Hash, FromPrimitive)]
 #[repr(u16)]
 pub enum Clusters {
     /// Bind Request.
@@ -14,4 +16,12 @@ pub enum Clusters {
     MgmtPermitJoiningReq = <MgmtPermitJoiningReq as Cluster>::ID,
     /// Network Address Request.
     NwkAddrReq = <NwkAddrReq as Cluster>::ID,
+}
+
+impl TryFrom<u16> for Clusters {
+    type Error = u16;
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        Self::from_u16(value).ok_or(value)
+    }
 }
