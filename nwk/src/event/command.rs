7fdb912849ce4e3cd9bb5a4316d6f359932e0792
Richard Neumann <neumann@paulmann.de>
2025-12-10 11:08:42 +0100

    Update Zcl command variant.

--- nwk/src/event/command.rs
+++ nwk/src/event/command.rs
@@ -1,9 +1,10 @@
 /// Commands received on the APS layer.
+#[expect(clippy::large_enum_variant)]
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub enum Command {
     /// A ZDP frame was received.
     // TODO: Is theis ZDP or ZDO?
     Zdp(zdp::Frames),
     /// A ZCL command was received.
-    Zcl(zcl::Frames),
+    Zcl(zcl::Frame<zcl::Cluster>),
 }
