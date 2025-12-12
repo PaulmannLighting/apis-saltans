7a8564a50eb6a1b59129dc29776d4fe337d3e70e
Richard Neumann <neumann@paulmann.de>
2025-12-12 14:28:55 +0100

    Add docs.

--- zdp/src/services/network_management/mgmt_nwk_update_req/scan_duration.rs
+++ zdp/src/services/network_management/mgmt_nwk_update_req/scan_duration.rs
@@ -7,22 +7,45 @@ use repr_discriminant::ReprDiscriminant;
 #[repr(u8)]
 pub enum ScanDuration {
     /// Scan duration of `<const> * (2^0)` symbols.
-    Zero { energy_scan: bool } = 0x00,
+    Zero {
+        /// Perform an energy scan if `true`.
+        energy_scan: bool,
+    } = 0x00,
     /// Scan duration of `<const> * (2^1)` symbols.
-    One { energy_scan: bool } = 0x01,
+    One {
+        /// Perform an energy scan if `true`.
+        energy_scan: bool,
+    } = 0x01,
     /// Scan duration of `<const> * (2^2)` symbols.
-    Two { energy_scan: bool } = 0x02,
+    Two {
+        /// Perform an energy scan if `true`.
+        energy_scan: bool,
+    } = 0x02,
     /// Scan duration of `<const> * (2^3)` symbols.
-    Three { energy_scan: bool } = 0x03,
+    Three {
+        /// Perform an energy scan if `true`.
+        energy_scan: bool,
+    } = 0x03,
     /// Scan duration of `<const> * (2^4)` symbols.
-    Four { energy_scan: bool } = 0x04,
+    Four {
+        /// Perform an energy scan if `true`.
+        energy_scan: bool,
+    } = 0x04,
     /// Scan duration of `<const> * (2^5)` symbols.
-    Five { energy_scan: bool } = 0x05,
+    Five {
+        /// Perform an energy scan if `true`.
+        energy_scan: bool,
+    } = 0x05,
     /// Channel change request.
-    ChannelChange { nwk_update_id: u8 } = 0xFE,
+    ChannelChange {
+        /// Network update ID.
+        nwk_update_id: u8,
+    } = 0xFE,
     /// `aps_channel_mask_list` and `nwk_manager_addr` attribute change request.
     AttributeChange {
+        /// Network update ID.
         nwk_update_id: u8,
+        /// Network manager address.
         nwk_manager_addr: u16,
     } = 0xFF,
 }
