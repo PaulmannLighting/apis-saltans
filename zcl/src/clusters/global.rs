d798558d2b77f74fd8c2890b1c56eb8afa3bda01
Richard Neumann <neumann@paulmann.de>
2025-12-16 15:30:40 +0100

    Remove `Cluster` implementation for `global::Command`.

--- zcl/src/clusters/global.rs
+++ zcl/src/clusters/global.rs
@@ -1,10 +1,9 @@
 //! General commands that are not specific to any cluster.
 
-use zigbee::Cluster;
 use zigbee_macros::ParseZclFrame;
 
 mod default_response;
-pub mod read_attributes;
+mod read_attributes;
 mod report_attributes;
 
 /// Available global commands.
@@ -18,8 +17,4 @@ pub enum Command {
     ReportAttributes(report_attributes::Command),
     /// Default Response command.
     DefaultResponse(default_response::DefaultResponse),
-}
-
-impl Cluster for Command {
-    const ID: u16 = 0x0000;
 }
