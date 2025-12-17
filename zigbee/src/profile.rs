ffab3bdf43ea024cc9d050e87de4b5b2c3385504
Richard Neumann <neumann@paulmann.de>
2025-12-17 16:35:15 +0100

    Update `Profile`.

--- zigbee/src/profile.rs
+++ zigbee/src/profile.rs
@@ -5,14 +5,20 @@ use num_traits::FromPrimitive;
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, FromPrimitive)]
 #[repr(u16)]
 pub enum Profile {
-    /// Profile Identifier for Unspecified Profile.
-    Unspecified = 0x0000,
+    /// Profile Identifier for Zigbee Device Profile(ZDP).
+    Network = 0x0000,
     /// Profile Identifier for Home Automation Profile.
     ZigbeeHomeAutomation = 0x0104,
+    /// Profile Identifier for Building Automation Profile.
+    BuildingAutomation = 0x0105,
+    /// Profile Identifier for Remote Control Profile.
+    RemoteControl = 0x0107,
+    /// Profile Identifier for Health Care Profile.
+    HealthCare = 0x0108,
     /// Profile Identifier for Smart Energy Profile.
     SmartEnergy = 0x0109,
     /// Profile Identifier for Light Link Profile.
-    Touchlink = 0xC05E,
+    TouchLink = 0xC05E,
 }
 
 impl TryFrom<u16> for Profile {
@@ -20,5 +26,11 @@ impl TryFrom<u16> for Profile {
 
     fn try_from(value: u16) -> Result<Self, Self::Error> {
         Self::from_u16(value).ok_or(value)
+    }
+}
+
+impl From<Profile> for u16 {
+    fn from(profile: Profile) -> Self {
+        profile as Self
     }
 }
