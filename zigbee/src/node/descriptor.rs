683e0e51009d7fd61e8e5d18c69c0ec299ea704e
Richard Neumann <neumann@paulmann.de>
2025-12-17 14:10:10 +0100

    Add methods to `Descriptor`.

--- zigbee/src/node/descriptor.rs
+++ zigbee/src/node/descriptor.rs
@@ -24,3 +24,77 @@ pub struct Descriptor {
     maximum_outgoing_transfer_size: u16,
     capability: u8,
 }
+
+impl Descriptor {
+    /// Creates a new `Descriptor`.
+    #[must_use]
+    pub const fn new(
+        flags: Flags,
+        mac_capability_flags: MacCapabilityFlags,
+        manufacturer_code: u16,
+        maximum_buffer_size: u8,
+        maximum_incoming_transfer_size: u16,
+        server_mask: ServerMask,
+        maximum_outgoing_transfer_size: u16,
+        capability: u8,
+    ) -> Self {
+        Self {
+            flags,
+            mac_capability_flags,
+            manufacturer_code,
+            maximum_buffer_size,
+            maximum_incoming_transfer_size,
+            server_mask,
+            maximum_outgoing_transfer_size,
+            capability,
+        }
+    }
+
+    /// Returns the flags.
+    #[must_use]
+    pub const fn flags(&self) -> &Flags {
+        &self.flags
+    }
+
+    /// Returns the MAC capability flags.
+    #[must_use]
+    pub const fn mac_capability_flags(&self) -> &MacCapabilityFlags {
+        &self.mac_capability_flags
+    }
+
+    /// Returns the manufacturer code.
+    #[must_use]
+    pub const fn manufacturer_code(&self) -> u16 {
+        self.manufacturer_code
+    }
+
+    /// Returns the maximum buffer size.
+    #[must_use]
+    pub const fn maximum_buffer_size(&self) -> u8 {
+        self.maximum_buffer_size
+    }
+
+    /// Returns the maximum incoming transfer size.
+    #[must_use]
+    pub const fn maximum_incoming_transfer_size(&self) -> u16 {
+        self.maximum_incoming_transfer_size
+    }
+
+    /// Returns the server mask.
+    #[must_use]
+    pub const fn server_mask(&self) -> &ServerMask {
+        &self.server_mask
+    }
+
+    /// Returns the maximum outgoing transfer size.
+    #[must_use]
+    pub const fn maximum_outgoing_transfer_size(&self) -> u16 {
+        self.maximum_outgoing_transfer_size
+    }
+
+    /// Returns the capability.
+    #[must_use]
+    pub const fn capability(&self) -> u8 {
+        self.capability
+    }
+}
