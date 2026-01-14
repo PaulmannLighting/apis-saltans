11100c2aabe3925fc557aab0ef634bfd2eb3deba
Richard Neumann <neumann@paulmann.de>
2026-01-14 13:22:55 +0100

    Implement `ColorControl` trait.

--- nwk/src/clusters.rs
+++ nwk/src/clusters.rs
@@ -1,7 +1,9 @@
 //! Traits and types for working with clusters.
 
 pub use self::binding::Binding;
+pub use self::color_control::ColorControl;
 pub use self::on_off::OnOff;
 
 mod binding;
+mod color_control;
 mod on_off;
---
+++ nwk/src/clusters/color_control.rs
@@ -0,0 +1,31 @@
+use zcl::lighting::color_control::MoveToColor;
+
+use crate::proxies::EndpointProxy;
+use crate::{Error, Proxy};
+
+/// Trait for Color Control cluster operations.
+pub trait ColorControl {
+    /// Move to the specified color (x, y) over the given transition time.
+    fn move_to_color(
+        &self,
+        color_x: u16,
+        color_y: u16,
+        transition_time: u16,
+    ) -> impl Future<Output = Result<u8, Error>>;
+}
+
+impl<T> ColorControl for EndpointProxy<'_, T>
+where
+    T: Proxy + Sync,
+{
+    async fn move_to_color(
+        &self,
+        color_x: u16,
+        color_y: u16,
+        transition_time: u16,
+    ) -> Result<u8, Error> {
+        self.zcl()
+            .unicast(MoveToColor::new(color_x, color_y, transition_time, 0, 0))
+            .await
+    }
+}
