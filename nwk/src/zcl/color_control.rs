141cd1d244708b1a3c03b176002a803af2f5490d
Richard Neumann <neumann@paulmann.de>
2026-01-20 13:29:51 +0100

    Use `bunt` to facilitate color setting.

--- nwk/src/zcl/color_control.rs
+++ nwk/src/zcl/color_control.rs
@@ -1,3 +1,4 @@
+use bunt::Xy;
 use zcl::lighting::color_control::MoveToColor;
 
 use crate::proxies::EndpointProxy;
@@ -6,26 +7,38 @@ use crate::{Error, Proxy};
 /// Trait for Color Control cluster operations.
 pub trait ColorControl {
     /// Move to the specified color (x, y) over the given transition time.
-    fn move_to_color(
+    fn move_to_xy(
         &self,
-        color_x: u16,
-        color_y: u16,
+        color: Xy,
         transition_time: u16,
     ) -> impl Future<Output = Result<u8, Error>> + Send;
+
+    /// Move to the specified color (x, y) over the given transition time.
+    fn move_to_color<T>(
+        &self,
+        color: T,
+        transition_time: u16,
+    ) -> impl Future<Output = Result<u8, Error>> + Send
+    where
+        T: Into<Xy>,
+    {
+        self.move_to_xy(color.into(), transition_time)
+    }
 }
 
 impl<T> ColorControl for EndpointProxy<'_, T>
 where
     T: Proxy + Sync,
 {
-    async fn move_to_color(
-        &self,
-        color_x: u16,
-        color_y: u16,
-        transition_time: u16,
-    ) -> Result<u8, Error> {
+    async fn move_to_xy(&self, color: Xy, transition_time: u16) -> Result<u8, Error> {
         self.zcl()
-            .unicast(MoveToColor::new(color_x, color_y, transition_time, 0, 0))
+            .unicast(MoveToColor::new(
+                color.x(),
+                color.y(),
+                transition_time,
+                0,
+                0,
+            ))
             .await
     }
 }
