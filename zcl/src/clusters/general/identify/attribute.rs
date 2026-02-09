51a890a52e3edbcacf91cd3bae04bbd4ce6dd837
Richard Neumann <neumann@paulmann.de>
2026-02-09 13:27:31 +0100

    Fix `Attribute::to_le_stream()`.

--- zcl/src/clusters/general/identify/attribute.rs
+++ zcl/src/clusters/general/identify/attribute.rs
@@ -2,6 +2,7 @@ use core::iter::Chain;
 
 use le_stream::{FromLeStreamTagged, ToLeStream};
 use repr_discriminant::ReprDiscriminant;
+use zigbee::types::{Type, Uint16};
 
 /// Attributes for the Identify cluster.
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -13,14 +14,14 @@ pub enum Attribute {
 }
 
 impl ToLeStream for Attribute {
-    type Iter = Chain<<u16 as ToLeStream>::Iter, <u16 as ToLeStream>::Iter>;
+    type Iter = Chain<<u16 as ToLeStream>::Iter, <Type as ToLeStream>::Iter>;
 
     fn to_le_stream(self) -> Self::Iter {
         match self {
             Self::IdentifyTime(time) => self
                 .discriminant()
                 .to_le_stream()
-                .chain(time.to_le_stream()),
+                .chain(Type::Uint16(Uint16::new(time)).to_le_stream()),
         }
     }
 }
