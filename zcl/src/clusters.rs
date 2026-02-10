06ae19f231fb6d47b865e6200a45eece13db61ec
Richard Neumann <neumann@paulmann.de>
2026-02-10 09:35:51 +0100

    Make module `measurement_and_sensing` public.

--- zcl/src/clusters.rs
+++ zcl/src/clusters.rs
@@ -7,7 +7,7 @@ use crate::{CommandId, Header, ParseFrameError, Scope};
 pub mod general;
 pub mod global;
 pub mod lighting;
-mod measurement_and_sensing;
+pub mod measurement_and_sensing;
 
 /// Available ZCL clusters.
 // TODO: Add all ZCL clusters.
