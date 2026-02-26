2365a2451510c6d9c747e84fcad36c0298bbf68f
Richard Neumann <neumann@paulmann.de>
2026-02-26 09:32:51 +0100

    Implement conversion between `Attribute` and `AttributeReport`.

--- zcl/src/clusters/general/on_off/attribute/report.rs
+++ zcl/src/clusters/general/on_off/attribute/report.rs
@@ -2,7 +2,10 @@
 
 use le_stream::FromLeStreamTagged;
 use repr_discriminant::ReprDiscriminant;
-use zigbee::types::Bool;
+use zigbee::types::{Bool, Type};
+
+use crate::ParseAttributeError;
+use crate::global::report_attributes::AttributeReport;
 
 /// Readable attributes for the On/Off cluster.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
@@ -11,4 +14,30 @@ use zigbee::types::Bool;
 pub enum Attribute {
     /// On/Off state of the device.
     OnOff(Bool) = 0x0000,
+}
+
+impl From<Attribute> for AttributeReport {
+    fn from(attribute: Attribute) -> Self {
+        #[expect(unsafe_code, clippy::undocumented_unsafe_blocks)]
+        // SAFETY: We provide the attribute's correct discriminant and appropriate `Type`.
+        match attribute {
+            Attribute::OnOff(on_off) => unsafe {
+                Self::new(attribute.discriminant(), Type::Boolean(on_off))
+            },
+        }
+    }
+}
+
+impl TryFrom<AttributeReport> for Attribute {
+    type Error = ParseAttributeError;
+
+    fn try_from(attribute_report: AttributeReport) -> Result<Self, Self::Error> {
+        match attribute_report.attribute_id() {
+            0x0000 => match attribute_report.into_data() {
+                Type::Boolean(on_off) => Ok(Self::OnOff(on_off)),
+                other => Err(ParseAttributeError::InvalidType(other)),
+            },
+            other => Err(ParseAttributeError::InvalidId(other)),
+        }
+    }
 }
