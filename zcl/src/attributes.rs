334462e818ee0d7f6aea1238fe972b406d0a7cbc
Richard Neumann <neumann@paulmann.de>
2026-03-02 13:27:28 +0100

    Require `ReprDiscriminant` on `ReadableAttribute`.

--- zcl/src/attributes.rs
+++ zcl/src/attributes.rs
@@ -1,7 +1,8 @@
+use repr_discriminant::ReprDiscriminant;
 use zigbee::Cluster;
 
 /// A trait to allow the reading of attributes by their respective IDs in a type-safe manner.
-pub trait ReadableAttribute: Cluster {
+pub trait ReadableAttribute: ReprDiscriminant + Cluster {
     /// The type of attribute, usually an enum, which is returned from the read.
     type ReadAttribute;
 }
