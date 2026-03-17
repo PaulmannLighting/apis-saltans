994f14583612124d667b6627d6c109501065056b
Richard Neumann <neumann@paulmann.de>
2026-03-17 09:14:59 +0100

    Export modules to prevent name clash with `command::Command`.

--- aps/src/lib.rs
+++ aps/src/lib.rs
@@ -1,11 +1,10 @@
 //! APS Frame definitions and utilities.
 
 pub use self::broadcast::Broadcast;
-pub use self::frame::acknowledgement::Frame as Ack;
-pub use self::frame::command::Frame as Command;
-pub use self::frame::data::Frame as Data;
+pub use self::frame::command::Command;
 pub use self::frame::{
-    AckFmt, Control, DeliveryMode, Destination, Extended, ExtendedControl, Fragmentation, FrameType,
+    AckFmt, Control, DeliveryMode, Destination, Extended, ExtendedControl, Fragmentation,
+    FrameType, acknowledgement, command, data,
 };
 
 mod broadcast;
