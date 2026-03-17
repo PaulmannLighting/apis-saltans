aec43eea42c743b4406eec22588b6717b9416cef
Richard Neumann <neumann@paulmann.de>
2026-03-17 13:53:23 +0100

    Remove TODO.

--- nwk/src/event/command.rs
+++ nwk/src/event/command.rs
@@ -3,7 +3,6 @@
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub enum Command {
     /// A ZDP frame was received.
-    // TODO: Is theis ZDP or ZDO?
     Zdp(zdp::Frame<zdp::Command>),
     /// A ZCL command was received.
     Zcl(zcl::Frame<zcl::Cluster>),
