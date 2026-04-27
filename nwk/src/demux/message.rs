46b28683e200508959e157aa5520f0799c6fc531
Richard Neumann <neumann@paulmann.de>
2026-04-27 10:25:49 +0200

    Implement `Message::subscribe()`.

--- nwk/src/demux/message.rs
+++ nwk/src/demux/message.rs
@@ -15,6 +15,17 @@ pub enum Message {
     },
 }
 
+impl Message {
+    /// Create a new subscription request.
+    #[must_use]
+    pub const fn subscribe(transaction: u8, response: Sender<Event>) -> Self {
+        Self::Subscribe {
+            transaction,
+            response,
+        }
+    }
+}
+
 impl From<Event> for Message {
     fn from(event: Event) -> Self {
         Self::Event(event)
