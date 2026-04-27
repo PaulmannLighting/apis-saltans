76f37a885f739e62b5e524d2546fc3c89622e4c0
Richard Neumann <neumann@paulmann.de>
2026-04-27 13:29:00 +0200

    [WIP] Implement Attribute reading.

--- nwk/src/transceiver.rs
+++ nwk/src/transceiver.rs
@@ -1,4 +1,8 @@
-use crate::{DemuxProxy, Proxy};
+use zcl::global::read_attributes;
+use zcl::{Cluster, Customizable, Global, HeaderFactory, global};
+use zigbee::Endpoint;
+
+use crate::{Command, DemuxProxy, Error, Event, Proxy};
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub struct Transceiver<T, R> {
@@ -22,14 +26,52 @@ where
     T: Proxy,
     R: DemuxProxy,
 {
-    fn zcl_transceive(&self) {
-        todo!(
-            "
-                1) Obtain ZCL sequence number `seq`.
-                2) Register handler with `seq`.
-                3) Send message with `seq`.
-                4) Receive response.
-            "
-        )
+    async fn read_attributes(
+        &self,
+        pan_id: u16,
+        endpoint: Endpoint,
+        cluster_id: u16,
+        attribute_ids: Box<[u16]>,
+        manufacturer_code: Option<u16>,
+    ) -> Result<Event, Error> {
+        let seq = self.tx_proxy.next_transaction_seq().await?;
+        let response = self.demux_proxy.subscribe(seq).await?;
+        self.tx_proxy
+            .unicast(
+                pan_id,
+                endpoint,
+                read_attributes::Command::new(attribute_ids)
+                    .for_cluster(cluster_id)
+                    .with_manufacturer_code(manufacturer_code)
+                    .frame(seq)
+                    .into(),
+            )
+            .await?;
+
+        if let Event::MessageReceived {
+            src_address,
+            aps_frame,
+        } = response.await?
+        {
+            if src_address != pan_id {
+                todo!("Handle unexpected source address.")
+            }
+
+            let (aps_header, payload) = aps_frame.into_parts();
+
+            if let Command::Zcl(frame) = payload {
+                let (zcl_header, payload) = frame.into_parts();
+
+                if let Cluster::Global(global::Command::ReadAttributesResponse(resp)) = payload {
+                    todo!("Handle read attributes response.")
+                } else {
+                    todo!("Handle unexpected payload.")
+                }
+            } else {
+                todo!("Handle unexpected payload.")
+            }
+        } else {
+            todo!("Handle unexpected response.")
+        }
     }
 }
