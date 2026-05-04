970b5baf9160dfa5fe37f0f84abf722c154cbd76
Richard Neumann <neumann@paulmann.de>
2026-05-04 11:38:02 +0200

    Refactor `demux::Error`.

--- nwk/src/demux/error.rs
+++ nwk/src/demux/error.rs
@@ -6,43 +6,50 @@ use tokio::sync::oneshot::error::RecvError;
 use crate::demux::Message;
 
 /// Error that can occur when sending to or receiving from a demultiplexer.
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[derive(Debug)]
 pub enum Error {
     /// Subscribing failed.
-    Subscribe,
+    Subscribe(SendError<Message>),
 
     /// Receiving failed.
-    Receive,
+    Receive(RecvError),
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Subscribe => write!(f, "Subscribe error"),
-            Self::Receive => write!(f, "Receive error"),
+            Self::Subscribe(error) => write!(f, "{error}"),
+            Self::Receive(error) => write!(f, "{error}"),
         }
     }
 }
 
-impl std::error::Error for Error {}
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Subscribe(error) => Some(error),
+            Self::Receive(error) => Some(error),
+        }
+    }
+}
 
 impl From<RecvError> for Error {
-    fn from(_: RecvError) -> Self {
-        Self::Receive
+    fn from(error: RecvError) -> Self {
+        Self::Receive(error)
     }
 }
 
 impl From<SendError<Message>> for Error {
-    fn from(_: SendError<Message>) -> Self {
-        Self::Receive
+    fn from(error: SendError<Message>) -> Self {
+        Self::Subscribe(error)
     }
 }
 
 impl From<Error> for crate::Error {
     fn from(error: Error) -> Self {
         match error {
-            Error::Subscribe => Self::ActorSend,
-            Error::Receive => Self::ActorReceive,
+            Error::Subscribe(_) => Self::ActorSend,
+            Error::Receive(_) => Self::ActorReceive,
         }
     }
 }
