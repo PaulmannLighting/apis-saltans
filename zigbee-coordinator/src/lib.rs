0c5f6d025526f0ae593b11fa32e1cd7d20dd59bc
Richard Neumann <neumann@paulmann.de>
2026-05-28 12:43:04 +0200

    Add new abstraction layer crates.

---
+++ zigbee-coordinator/src/lib.rs
@@ -0,0 +1,6 @@
+//! Zigbee coordinator API.
+//!
+//! This library provides a fully abstracted interface to expose an interface to communicate with
+//! a Zigbee coordinator regardless of the underlying hardware.
+//!
+//! TODO: This shall replace `zigbee-nwk`.
---
+++ zigbee-hw/src/lib.rs
@@ -0,0 +1,4 @@
+//! Zigbee hardware API.
+//!
+//! This library provides a unified interface to implement Zigbee coordinator functionality for
+//! Zigbee hardware (NCP) drivers.
