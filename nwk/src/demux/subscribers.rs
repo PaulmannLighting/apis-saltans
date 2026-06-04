d722d2cb33abba5e063a40df1b59c02ff0def72a
Richard Neumann <neumann@paulmann.de>
2026-06-04 15:51:20 +0200

    Remove obsolete `nwk` subsystem.

--- nwk/src/demux/subscribers.rs
+++
@@ -1,8 +0,0 @@
-use std::collections::BTreeMap;
-
-use tokio::sync::oneshot::Sender;
-
-use crate::Event;
-
-/// A map of subscribers.
-pub type Subscribers = BTreeMap<u8, Sender<Event>>;
--- nwk/src/zcl/proxies.rs
+++
@@ -1,7 +0,0 @@
-//! ZCL proxies.
-
-pub use self::device::Proxy as DeviceProxy;
-pub use self::endpoint::Proxy as EndpointProxy;
-
-mod device;
-mod endpoint;
