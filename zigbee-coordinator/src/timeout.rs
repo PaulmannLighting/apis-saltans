5c460525df801ad96886fdb3a4b463fabc4e2030
Richard Neumann <neumann@paulmann.de>
2026-06-08 14:58:23 +0200

    Change default timeout to 3 seconds.

--- zigbee-coordinator/src/timeout.rs
+++ zigbee-coordinator/src/timeout.rs
@@ -7,7 +7,7 @@ use tokio::time::error::Elapsed;
 
 /// Timeout for ZCL responses.
 #[env_item("ZIGBEE_COORDINATOR_ZCL_RESPONSE_TIMEOUT_SECS")]
-const ZCL_RESPONSE_TIMEOUT_SECS: u64 = 10;
+const ZCL_RESPONSE_TIMEOUT_SECS: u64 = 3;
 const ZCL_RESPONSE_TIMEOUT: Duration = Duration::from_secs(ZCL_RESPONSE_TIMEOUT_SECS);
 
 /// Extension trait to add a timeout while waiting for a future.
