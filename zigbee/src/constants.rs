180e022de11b7bd76d05207f2cd372d937b8ffb1
Richard Neumann <neumann@paulmann.de>
2026-06-09 14:20:15 +0200

    Fix `U8_CAPACITY`.

--- zigbee/src/constants.rs
+++ zigbee/src/constants.rs
@@ -3,5 +3,5 @@
 /// Amount of deci-seconds per millisecond.
 pub const DECI_SECONDS_PER_MILLISECOND: u64 = 100;
 
-/// Maximum size of the bytes array.
-pub const U8_CAPACITY: usize = (u8::MAX - 1) as usize;
+/// Maximum size of a byte-sized array.
+pub const U8_CAPACITY: usize = u8::MAX as usize;
