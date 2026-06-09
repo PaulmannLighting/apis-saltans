5f3ef1ae00eeac624a9d426e84598de37fce0977
Richard Neumann <neumann@paulmann.de>
2026-06-09 15:30:13 +0200

    [WIP] Implement device and endpoint types for network manager.

--- zigbee-coordinator/src/network_manager/device.rs
+++ zigbee-coordinator/src/network_manager/device.rs
@@ -1,2 +1,14 @@
+use std::collections::BTreeMap;
+
+use zigbee::Address;
+
+pub use self::endpoint::Endpoint;
+
+mod endpoint;
+
+/// A Zigbee network device.
 #[derive(Debug)]
-pub struct Device {}
+pub struct Device {
+    address: Address,
+    endpoints: BTreeMap<zigbee::Endpoint, Endpoint>,
+}
