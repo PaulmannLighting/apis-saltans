2e970657b9c45da3c2bbc60e1f323aa670709a83
Richard Neumann <neumann@paulmann.de>
2026-06-09 14:28:19 +0200

    Set `LenT` to `u8` on `GroupList`.

--- zcl/src/clusters/general/groups/types.rs
+++ zcl/src/clusters/general/groups/types.rs
@@ -1,4 +1,4 @@
 use zigbee::types::{Uint8, Uint16};
 
 /// A list of group IDs.
-pub type GroupList = heapless::Vec<Uint16, { Uint8::MAX.as_usize() }>;
+pub type GroupList = heapless::Vec<Uint16, { Uint8::MAX.as_usize() }, u8>;
