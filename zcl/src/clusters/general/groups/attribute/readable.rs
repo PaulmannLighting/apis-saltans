5cbd9100deb18d71bbb77fa7966d4f9fbfb53a82
Richard Neumann <neumann@paulmann.de>
2026-06-10 12:27:19 +0200

    Remove obsolete use of `Parsable`.

--- zcl/src/clusters/general/groups/attribute/readable.rs
+++ zcl/src/clusters/general/groups/attribute/readable.rs
@@ -2,9 +2,8 @@
 
 use core::iter::Chain;
 
-use le_stream::{FromLeStreamTagged, ToLeStream};
+use le_stream::ToLeStream;
 use repr_discriminant::ReprDiscriminant;
-use zigbee::Parsable;
 use zigbee::types::Type;
 
 use crate::general::groups::NameSupport;
@@ -12,10 +11,10 @@ use crate::general::groups::NameSupport;
 /// Available attribute for the `Groups` cluster.
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[repr(u16)]
-#[derive(ReprDiscriminant, FromLeStreamTagged)]
+#[derive(ReprDiscriminant)]
 pub enum Attribute {
     /// Flag indicating whether the group name is supported by the device.
-    NameSupport(Parsable<u8, NameSupport>) = 0x0000,
+    NameSupport(NameSupport) = 0x0000,
 }
 
 impl ToLeStream for Attribute {
@@ -26,7 +25,7 @@ impl ToLeStream for Attribute {
             Self::NameSupport(name_support) => self
                 .discriminant()
                 .to_le_stream()
-                .chain(Type::Map8(name_support.into_src()).to_le_stream()),
+                .chain(Type::Map8(name_support.into()).to_le_stream()),
         }
     }
 }
