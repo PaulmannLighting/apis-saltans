b765df6e395f37126b9b39564cc21cf982aae853
Richard Neumann <neumann@paulmann.de>
2026-06-13 14:57:26 +0200

    Fix typo.

--- zigbee/src/traits/expect_response.rs
+++ zigbee/src/traits/expect_response.rs
@@ -1,4 +1,4 @@
-/// Trait for commands that are expected to respond with a specfic response type.
+/// Trait for commands that are expected to respond with a specific response type.
 pub trait ExpectResponse<T>: Into<T> {
     /// The response type.
     type Response: TryFrom<T>;
