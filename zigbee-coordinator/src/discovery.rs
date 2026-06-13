c4967a3e6192af6e38d182b135f4f86a89d89b01
Richard Neumann <neumann@paulmann.de>
2026-06-13 13:19:07 +0200

    Split discovery stages into separate impl blocks.

--- zigbee-coordinator/src/discovery.rs
+++ zigbee-coordinator/src/discovery.rs
@@ -1,7 +1,7 @@
 use std::collections::{BTreeMap, BTreeSet};
 use std::time::Duration;
 
-use log::{error, trace, warn};
+use log::{error, info, trace, warn};
 use tokio::spawn;
 use tokio::sync::mpsc::{Receiver, Sender, channel};
 use tokio::time::sleep;
@@ -76,88 +76,17 @@ impl Actor {
     fn handle_event(&mut self, event: Event) {
         match event {
             Event::DeviceJoined(address) => self.handle_join(address),
-            Event::DeviceLeft(address) => self.handle_leave(&address),
+            Event::DeviceRejoined { address, secured } => {
+                info!("Device rejoined: {address:?}, secured: {secured}");
+                self.handle_join(address);
+            }
             _ => trace!("Unhandled event: {event:?}"),
         }
     }
+}
 
-    fn handle_active_ep_rsp(&mut self, address: Address, result: Result<ActiveEpRsp, Error>) {
-        match result {
-            Ok(active_ep_rsp) => match active_ep_rsp.status() {
-                Ok(Status::Success) => {
-                    self.update_endpoints(&address, active_ep_rsp);
-                    return;
-                }
-                Ok(error) => {
-                    error!("Failed to get active endpoints for {address:?}: {error:?}");
-                }
-                Err(code) => {
-                    warn!("Failed to get active endpoints for {address:?}: {code:#06X}");
-                }
-            },
-            Err(error) => {
-                warn!("Failed to get active endpoints for {address:?}: {error:?}");
-            }
-        }
-
-        self.schedule_endpoint_discovery(address, Some(RESCHEDULE_DELAY));
-    }
-
-    fn handle_simple_desc_rsp(&mut self, address: Address, result: Result<SimpleDescRsp, Error>) {
-        let Ok(simple_desc_rsp) =
-            result.inspect_err(|error| error!("Failed to get simple descriptor: {error:?}"))
-        else {
-            return;
-        };
-
-        let Ok(Status::Success) = simple_desc_rsp.status() else {
-            error!(
-                "Failed to get simple descriptor: {:?}",
-                simple_desc_rsp.status()
-            );
-            return;
-        };
-
-        let mut old_devices = self.device_endpoints.get_mut(&address);
-        let device = self.discovered_endpoints.entry(address).or_default();
-
-        for descriptor in simple_desc_rsp.into_descriptors() {
-            let zigbee::Endpoint::Application(application) = descriptor.endpoint() else {
-                //error!("Failed to parse endpoint: {:#06X}", descriptor.endpoint());
-                continue;
-            };
-
-            if let Some(old_device) = old_devices.as_mut() {
-                old_device.remove(&application);
-            }
-
-            device.insert(application, descriptor.into());
-        }
-    }
-
-    fn update_endpoints(&mut self, address: &Address, active_ep_rsp: ActiveEpRsp) {
-        let endpoints: BTreeSet<_> = active_ep_rsp
-            .into_iter()
-            .filter_map(|endpoint| {
-                if let zigbee::Endpoint::Application(endpoint) = endpoint {
-                    Some(endpoint)
-                } else {
-                    None
-                }
-            })
-            .collect();
-
-        self.device_endpoints
-            .entry(address.clone())
-            .or_default()
-            .extend(endpoints.clone());
-
-        for endpoint in &endpoints {
-            self.discovered_devices.remove(&address);
-            self.schedule_descriptor_discovery(address.clone(), *endpoint, None);
-        }
-    }
-
+/// Stage 1: Device discovery.
+impl Actor {
     fn handle_join(&mut self, address: Address) {
         self.discovered_devices.insert(address.clone());
         self.schedule_endpoint_discovery(address, None);
@@ -180,6 +109,89 @@ impl Actor {
                 .await
                 .unwrap_or_else(|error| error!("Failed to send ActiveEpReq: {error:?}"));
         });
+    }
+}
+
+/// Stage 2: Endpoint discovery.
+impl Actor {
+    fn handle_active_ep_rsp(&mut self, address: Address, result: Result<ActiveEpRsp, Error>) {
+        match result {
+            Ok(active_ep_rsp) => match active_ep_rsp.status() {
+                Ok(Status::Success) => {
+                    self.discovered_devices.remove(&address);
+                    self.update_endpoints(&address, active_ep_rsp);
+                    return;
+                }
+                Ok(error) => {
+                    error!("Failed to get active endpoints for {address:?}: {error:?}");
+                }
+                Err(code) => {
+                    warn!("Failed to get active endpoints for {address:?}: {code:#06X}");
+                }
+            },
+            Err(error) => {
+                warn!("Failed to get active endpoints for {address:?}: {error:?}");
+            }
+        }
+
+        self.schedule_endpoint_discovery(address, Some(RESCHEDULE_DELAY));
+    }
+
+    fn update_endpoints(&mut self, address: &Address, active_ep_rsp: ActiveEpRsp) {
+        let endpoints: BTreeSet<_> = active_ep_rsp
+            .into_iter()
+            .filter_map(|endpoint| {
+                if let zigbee::Endpoint::Application(endpoint) = endpoint {
+                    Some(endpoint)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        self.device_endpoints
+            .entry(address.clone())
+            .or_default()
+            .extend(endpoints.clone());
+
+        for endpoint in &endpoints {
+            self.schedule_descriptor_discovery(address.clone(), *endpoint, None);
+        }
+    }
+}
+
+/// Stage 3: Descriptor discovery.
+impl Actor {
+    fn handle_simple_desc_rsp(&mut self, address: Address, result: Result<SimpleDescRsp, Error>) {
+        let Ok(simple_desc_rsp) =
+            result.inspect_err(|error| error!("Failed to get simple descriptor: {error:?}"))
+        else {
+            return;
+        };
+
+        let Ok(Status::Success) = simple_desc_rsp.status() else {
+            error!(
+                "Failed to get simple descriptor: {:?}",
+                simple_desc_rsp.status()
+            );
+            return;
+        };
+
+        let mut old_devices = self.device_endpoints.get_mut(&address);
+        let device = self.discovered_endpoints.entry(address).or_default();
+
+        for descriptor in simple_desc_rsp.into_descriptors() {
+            let zigbee::Endpoint::Application(application) = descriptor.endpoint() else {
+                error!("Failed to parse endpoint: {}", descriptor.endpoint());
+                continue;
+            };
+
+            if let Some(old_device) = old_devices.as_mut() {
+                old_device.remove(&application);
+            }
+
+            device.insert(application, descriptor.into());
+        }
     }
 
     fn schedule_descriptor_discovery(
@@ -206,9 +218,5 @@ impl Actor {
                 .await
                 .unwrap_or_else(|error| error!("Failed to send SimpleDescRsp: {error:?}"));
         });
-    }
-
-    fn handle_leave(&mut self, address: &Address) {
-        todo!("Handle device leave");
     }
 }
