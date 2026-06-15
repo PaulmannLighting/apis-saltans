d78a0d1f39ecb8e1e255564ae096aed58b0f054a
Richard Neumann <neumann@paulmann.de>
2026-06-15 13:03:14 +0200

    Implement conversion traits for `ClusterId`.

--- zigbee/src/cluster/cluster_id.rs
+++ zigbee/src/cluster/cluster_id.rs
@@ -1,6 +1,9 @@
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
 /// Known ZCL cluster identifiers defined in this crate's `clusters` module.
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, FromPrimitive)]
 #[repr(u16)]
 pub enum ClusterId {
     /// Basic cluster.
@@ -36,5 +39,19 @@ impl ClusterId {
     #[must_use]
     pub const fn as_u16(self) -> u16 {
         self as u16
+    }
+}
+
+impl From<ClusterId> for u16 {
+    fn from(cluster_id: ClusterId) -> Self {
+        cluster_id.as_u16()
+    }
+}
+
+impl TryFrom<u16> for ClusterId {
+    type Error = u16;
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        Self::from_u16(value).ok_or(value)
     }
 }
