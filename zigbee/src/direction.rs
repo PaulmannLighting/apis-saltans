c967d1fbbe7901d7954ea0cdeaec8aa5b930c14a
Richard Neumann <neumann@paulmann.de>
2026-06-16 15:35:29 +0200

    Update docs and add spacing between variants.

--- zigbee/src/direction.rs
+++ zigbee/src/direction.rs
@@ -2,8 +2,9 @@
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 #[repr(u8)]
 pub enum Direction {
-    /// Frame is sent from client side to server side.
+    /// Frame is sent from the client side to the server side.
     ClientToServer = 0x00,
-    /// Frame is sent from server side to client side.
+
+    /// Frame is sent from the server side to the client side.
     ServerToClient = 0x01,
 }
