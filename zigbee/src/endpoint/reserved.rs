17d482892da3980f0eae7c123a4a759cbebeb558
Richard Neumann <neumann@paulmann.de>
2026-06-17 21:52:46 +0200

    Disallow construction of `Reserved` outside of crate.

--- zigbee/src/endpoint/reserved.rs
+++ zigbee/src/endpoint/reserved.rs
@@ -12,33 +12,6 @@ impl Reserved {
 
     /// The maximum valid reserved endpoint ID.
     pub const MAX: u8 = 254;
-
-    /// Create a new `Reserved` endpoint ID if the given ID is valid.
-    #[must_use]
-    pub const fn new(id: u8) -> Option<Self> {
-        if id >= Self::MIN && id <= Self::MAX {
-            Some(Self(id))
-        } else {
-            None
-        }
-    }
-
-    /// Create a new `Reserved` endpoint ID, clamping the given ID to the valid range.
-    #[must_use]
-    pub fn new_clamped(id: u8) -> Self {
-        Self(id.clamp(Self::MIN, Self::MAX))
-    }
-
-    /// Create a new `Reserved` endpoint ID without checking validity.
-    ///
-    /// # Safety
-    ///
-    /// The caller must ensure that the given ID is within the valid range (241..=254).
-    #[expect(unsafe_code)]
-    #[must_use]
-    pub const unsafe fn new_unchecked(id: u8) -> Self {
-        Self(id)
-    }
 }
 
 impl Display for Reserved {
@@ -62,13 +35,5 @@ impl UpperHex for Reserved {
 impl From<Reserved> for u8 {
     fn from(endpoint: Reserved) -> Self {
         endpoint.0
-    }
-}
-
-impl TryFrom<u8> for Reserved {
-    type Error = u8;
-
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        Self::new(value).ok_or(value)
     }
 }
