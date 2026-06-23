8c2cbfdb244139ee112c9d127a1492f24d881530
Richard Neumann <neumann@paulmann.de>
2026-06-23 20:47:25 +0200

    Add spacing around enum vaiants.

--- zigbee-hw/src/event.rs
+++ zigbee-hw/src/event.rs
@@ -10,14 +10,19 @@ mod command;
 pub enum Event {
     /// The network is up and running.
     NetworkUp,
+
     /// The network is down.
     NetworkDown,
+
     /// The network has been opened for new joins.
     NetworkOpened,
+
     /// The network has been closed for new joins.
     NetworkClosed,
+
     /// A new device has joined the network.
     DeviceJoined(Address),
+
     /// A device has rejoined the network.
     DeviceRejoined {
         /// The address of the joined device.
@@ -25,8 +30,10 @@ pub enum Event {
         /// Whether the rejoining was secured.
         secured: bool,
     },
+
     /// A device has left the network.
     DeviceLeft(Address),
+
     /// Message received from a device.
     MessageReceived {
         /// The PAN ID of the sender.
