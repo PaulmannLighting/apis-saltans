e6cbbfff5245736dec59cfcb65375e636e6aacc9
Richard Neumann <neumann@paulmann.de>
2026-06-24 00:03:49 +0200

    Cargo fmt.

--- zcl/src/clusters/global.rs
+++ zcl/src/clusters/global.rs
@@ -199,7 +199,10 @@ impl ToLeStream for Command {
 }
 
 mod iterator {
-    use super::{ToLeStream, read_attributes, write_attributes, write_attributes_undivided, write_attributes_no_response, report_attributes, default_response, Box, configure_reporting};
+    use super::{
+        Box, ToLeStream, configure_reporting, default_response, read_attributes, report_attributes,
+        write_attributes, write_attributes_no_response, write_attributes_undivided,
+    };
 
     pub enum Iter {
         ReadAttributes(<read_attributes::Command as ToLeStream>::Iter),
