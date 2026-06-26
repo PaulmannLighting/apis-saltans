251140e79db0c146dc6962f16db2a54f2415b9c0
Richard Neumann <neumann@paulmann.de>
2026-06-26 13:22:26 +0200

    Revamp `Deciseconds`.

--- zigbee/src/units/deciseconds.rs
+++ zigbee/src/units/deciseconds.rs
@@ -6,69 +6,36 @@ const MILLIS_PER_DECISECOND: u64 = 100;
 
 /// Type to represent a duration in 1/10ths of a second.
 #[derive(Clone, Copy, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
-pub struct Deciseconds<T>(T);
+pub struct Deciseconds(Uint16);
 
-impl<T> Deciseconds<T> {
-    /// Create a new [`Deciseconds`] from the given value.
-    #[must_use]
-    pub const fn new(value: T) -> Self {
-        Self(value)
-    }
-
+impl Deciseconds {
     /// Get the inner value.
     #[must_use]
-    pub fn into_inner(self) -> T {
+    pub const fn into_inner(self) -> Uint16 {
         self.0
     }
 }
 
-impl<T> Deciseconds<T>
-where
-    T: Copy,
-{
-    /// Return a copy of the inner value.
-    #[must_use]
-    pub const fn copy_inner(self) -> T {
-        self.0
-    }
-}
-
-impl TryFrom<Duration> for Deciseconds<u16> {
+impl TryFrom<Duration> for Deciseconds {
     type Error = Duration;
 
     fn try_from(duration: Duration) -> Result<Self, Self::Error> {
-        Ok(Self(
-            u16::try_from(duration.as_millis() / u128::from(MILLIS_PER_DECISECOND))
-                .map_err(|_| duration)?,
-        ))
+        u16::try_from(duration.as_millis() / u128::from(MILLIS_PER_DECISECOND))
+            .map_err(|_| duration)?
+            .try_into()
+            .map_err(|()| duration)
+            .map(Self)
     }
 }
 
-impl TryFrom<Duration> for Deciseconds<Uint16> {
-    type Error = Duration;
+impl TryFrom<Deciseconds> for Duration {
+    type Error = Deciseconds;
 
-    fn try_from(duration: Duration) -> Result<Self, Self::Error> {
-        Ok(Self(
-            Deciseconds::<u16>::try_from(duration)?
-                .0
-                .try_into()
-                .map_err(|()| duration)?,
-        ))
-    }
-}
-
-impl From<Deciseconds<u16>> for Duration {
-    fn from(deciseconds: Deciseconds<u16>) -> Self {
-        Self::from_millis(u64::from(deciseconds.0) * MILLIS_PER_DECISECOND)
-    }
-}
-
-impl TryFrom<Deciseconds<Uint16>> for Duration {
-    type Error = Deciseconds<Uint16>;
-
-    fn try_from(value: Deciseconds<Uint16>) -> Result<Self, Self::Error> {
-        Ok(Self::from_millis(
-            u64::from(Option::<u16>::from(value.0).ok_or(value)?) * MILLIS_PER_DECISECOND,
-        ))
+    fn try_from(value: Deciseconds) -> Result<Self, Self::Error> {
+        u16::try_from(value.0)
+            .map_err(|()| value)
+            .map(u64::from)
+            .map(|deciseconds| deciseconds * MILLIS_PER_DECISECOND)
+            .map(Self::from_millis)
     }
 }
