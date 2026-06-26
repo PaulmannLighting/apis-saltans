5cf4416fac4a6d7169b33ea0c736e45dc722efb4
Richard Neumann <neumann@paulmann.de>
2026-06-26 12:28:47 +0200

    Move `Deciseconds` into zigbee core crate.

--- zigbee/src/units.rs
+++ zigbee/src/units.rs
@@ -1,5 +1,7 @@
 //! Commonly used units of measurement.
 
+pub use self::deciseconds::Deciseconds;
 pub use self::mireds::Mireds;
 
+mod deciseconds;
 mod mireds;
---
+++ zigbee/src/units/deciseconds.rs
@@ -0,0 +1,63 @@
+use core::time::Duration;
+
+use crate::types::Uint16;
+
+const MILLIS_PER_DECISECOND: u64 = 100;
+
+/// Type to represent a duration in 1/10ths of a second.
+#[derive(Debug, Default, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct Deciseconds<T>(T);
+
+impl<T> Deciseconds<T> {
+    /// Create a new [`Deciseconds`] from the given value.
+    #[must_use]
+    pub const fn new(value: T) -> Self {
+        Self(value)
+    }
+
+    /// Get the inner value.
+    #[must_use]
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
+
+impl TryFrom<Duration> for Deciseconds<u16> {
+    type Error = Duration;
+
+    fn try_from(duration: Duration) -> Result<Self, Self::Error> {
+        Ok(Self(
+            u16::try_from(duration.as_millis() / u128::from(MILLIS_PER_DECISECOND))
+                .map_err(|_| duration)?,
+        ))
+    }
+}
+
+impl TryFrom<Duration> for Deciseconds<Uint16> {
+    type Error = Duration;
+
+    fn try_from(duration: Duration) -> Result<Self, Self::Error> {
+        Ok(Self(
+            Deciseconds::<u16>::try_from(duration)?
+                .0
+                .try_into()
+                .map_err(|_| duration)?,
+        ))
+    }
+}
+
+impl From<Deciseconds<u16>> for Duration {
+    fn from(deciseconds: Deciseconds<u16>) -> Self {
+        Self::from_millis(u64::from(deciseconds.0) * MILLIS_PER_DECISECOND)
+    }
+}
+
+impl TryFrom<Deciseconds<Uint16>> for Duration {
+    type Error = Deciseconds<Uint16>;
+
+    fn try_from(value: Deciseconds<Uint16>) -> Result<Self, Self::Error> {
+        Ok(Self::from_millis(
+            u64::from(Option::<u16>::from(value.0).ok_or(value)?) * MILLIS_PER_DECISECOND,
+        ))
+    }
+}
