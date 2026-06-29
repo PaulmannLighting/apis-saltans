3a4b2dbe421306e3884171355ddac20c98a64dbc
Richard Neumann <neumann@paulmann.de>
2026-06-29 12:30:06 +0200

    Add logging to APS defragmentation.

--- aps/src/transactions.rs
+++ aps/src/transactions.rs
@@ -1,5 +1,7 @@
 use std::collections::BTreeMap;
 
+use log::{debug, error, warn};
+
 use crate::ExtendedControl;
 use crate::data::{Frame, Header};
 
@@ -21,18 +23,26 @@ impl Transactions {
     /// Add a frame to the transaction, returning the defragmented frame if possible.
     #[must_use]
     pub fn add(&mut self, frame: Frame<Vec<u8>>) -> Option<Frame<Vec<u8>>> {
+        debug!("Received APS frame: {frame:?}");
+
         let Some(extended) = frame.header().extended() else {
+            debug!("APS frame has no extended header.");
             return Some(frame);
         };
 
+        debug!("APS frame has extended header: {extended:?}");
+
         if extended.control().contains(ExtendedControl::FIRST_FRAGMENT) {
+            debug!("APS frame is first fragment.");
+
             let Some(blocks) = extended.block_number() else {
+                warn!("APS frame has no block number.");
                 return Some(frame);
             };
 
             let (header, payload) = frame.into_parts();
-            let mut chunks = Vec::with_capacity(blocks.into());
-            chunks.push(payload);
+            let mut chunks = vec![Vec::new(); blocks.into()];
+            chunks[0] = payload;
 
             self.frames
                 .insert(header.counter(), (blocks, header, chunks));
@@ -43,27 +53,33 @@ impl Transactions {
             .control()
             .contains(ExtendedControl::FOLLOWUP_FRAGMENT)
         {
+            debug!("APS frame is not a follow-up fragment.");
             return Some(frame);
         }
 
         let Some(index) = extended.block_number() else {
+            warn!("APS frame has no block number.");
             return Some(frame);
         };
 
+        debug!("APS frame is follow-up fragment: block_number={index}");
         let (header, payload) = frame.into_parts();
         let (blocks, mut header, mut chunks) = self.frames.remove(&header.counter())?;
 
         if let Some(entry) = chunks.get_mut(usize::from(index)) {
             entry.extend(payload);
         } else {
+            error!("Invalid block number: {index}");
             return None;
         }
 
         if chunks.len() == blocks.saturating_sub(1).into() {
+            debug!("APS frame complete: blocks={blocks}");
             header.drop_extended();
             return Some(Frame::raw(header, chunks.into_iter().flatten().collect()));
         }
 
+        debug!("APS frame incomplete: blocks={blocks}");
         self.frames
             .insert(header.counter(), (blocks, header, chunks));
         None
