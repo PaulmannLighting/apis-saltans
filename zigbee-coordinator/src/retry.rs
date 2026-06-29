0a653b176619c5fbc38e77f2212ea26051212779
Richard Neumann <neumann@paulmann.de>
2026-06-29 20:45:12 +0200

    Yield duration on retries.

--- zigbee-coordinator/src/retry.rs
+++ zigbee-coordinator/src/retry.rs
@@ -1,38 +1,34 @@
 use std::time::Duration;
 
-use tokio::time::sleep;
-
 /// Utility struct to facilitate retrying operations.
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct Retry {
-    max: usize,
+    max: u32,
     delay: Duration,
 }
 
 impl Retry {
     /// Create a new instance of `Retry`.
     #[must_use]
-    pub const fn new(max: usize, delay: Duration) -> Self {
+    pub const fn new(max: u32, delay: Duration) -> Self {
         Self { max, delay }
     }
 
     /// Return `true` as long as the number of retries is less than the maximum.
     #[must_use]
-    pub async fn retry(&self, retries: &mut usize) -> bool {
+    pub async fn retry(&self, retries: &mut u32) -> Option<Duration> {
         if *retries > self.max {
-            return false;
+            return None;
         }
 
-        if *retries > 0 {
-            sleep(self.delay).await;
-        }
+        let delay = self.delay * *retries;
 
         if let Some(next) = retries.checked_add(1) {
             *retries = next;
         } else {
-            return false;
+            return None;
         }
 
-        true
+        Some(delay)
     }
 }
