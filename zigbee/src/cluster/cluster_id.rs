c2af47409b583a9547456e2b391584c5d28e925f
Richard Neumann <neumann@paulmann.de>
2026-06-29 08:05:34 +0200

    Add spacing between enum variants.

--- zigbee/src/cluster/cluster_id.rs
+++ zigbee/src/cluster/cluster_id.rs
@@ -10,28 +10,40 @@ use num_traits::FromPrimitive;
 pub enum ClusterId {
     /// Basic cluster.
     Basic = 0x0000,
+
     /// Power configuration cluster.
     PowerConfiguration = 0x0001,
+
     /// Device temperature configuration cluster.
     DeviceTemperatureConfiguration = 0x0002,
+
     /// Identify cluster.
     Identify = 0x0003,
+
     /// Groups cluster.
     Groups = 0x0004,
+
     /// Scenes cluster.
     Scenes = 0x0005,
+
     /// On/Off cluster.
     OnOff = 0x0006,
+
     /// Level control cluster.
     Level = 0x0008,
+
     /// Alarms cluster.
     Alarms = 0x0009,
+
     /// Time cluster.
     Time = 0x000A,
+
     /// Color control cluster.
     ColorControl = 0x0300,
+
     /// Illuminance measurement cluster.
     IlluminanceMeasurement = 0x0400,
+
     /// Illuminance level sensing cluster.
     IlluminanceLevelSensing = 0x0401,
 }
