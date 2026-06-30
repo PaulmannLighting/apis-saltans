6ef4189e0eaaf696ab63d5d750ade98dd7eb9b0d
Richard Neumann <neumann@paulmann.de>
2026-06-30 11:15:11 +0200

    Remove obsolete constant.

--- zigbee-coordinator/src/timeout.rs
+++ zigbee-coordinator/src/timeout.rs
@@ -2,14 +2,7 @@
 
 use std::time::Duration;
 
-use const_env::env_item;
-
 use crate::Error;
-
-/// Timeout for ZCL responses.
-#[env_item("ZIGBEE_COORDINATOR_ZCL_RESPONSE_TIMEOUT_SECS")]
-const ZCL_RESPONSE_TIMEOUT_SECS: u64 = 10;
-const ZCL_RESPONSE_TIMEOUT: Duration = Duration::from_secs(ZCL_RESPONSE_TIMEOUT_SECS);
 
 /// Extension trait to add a timeout while waiting for a future.
 pub trait Timeout<S, E> {
