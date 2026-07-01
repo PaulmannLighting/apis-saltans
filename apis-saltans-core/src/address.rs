e7d07d52a9bd29c9e0c8f3971564829f59f822f0
Richard Neumann <neumann@paulmann.de>
2026-07-01 20:07:09 +0200

    Rename feature to represent IEEE address as string serialization.
    # Conflicts:
    #	apis-saltans-coordinator/Cargo.toml
    #	apis-saltans-core/Cargo.toml

--- apis-saltans-core/src/address.rs
+++ apis-saltans-core/src/address.rs
@@ -5,11 +5,18 @@ use core::fmt::{self, Display};
 use macaddr::MacAddr8;
 
 /// Zigbee device addressing modes.
-#[cfg_attr(feature = "serde_with", cfg_eval::cfg_eval, serde_with::serde_as)]
+#[cfg_attr(
+    feature = "ieee_address_as_str",
+    cfg_eval::cfg_eval,
+    serde_with::serde_as
+)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct Address {
-    #[cfg_attr(feature = "serde_with", serde_as(as = "serde_with::DisplayFromStr"))]
+    #[cfg_attr(
+        feature = "ieee_address_as_str",
+        serde_as(as = "serde_with::DisplayFromStr")
+    )]
     ieee_address: MacAddr8,
     short_id: u16,
 }
