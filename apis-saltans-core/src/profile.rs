2b05121d440ab45a2d98f80e0c7ae31d14733d68
Richard Neumann <neumann@paulmann.de>
2026-07-01 19:55:10 +0200

    Add spacing between enum variants.

--- apis-saltans-core/src/profile.rs
+++ apis-saltans-core/src/profile.rs
@@ -7,16 +7,22 @@ use num_traits::FromPrimitive;
 pub enum Profile {
     /// Profile Identifier for Zigbee Device Profile(ZDP).
     Network = 0x0000,
+
     /// Profile Identifier for Home Automation Profile.
     ZigbeeHomeAutomation = 0x0104,
+
     /// Profile Identifier for Building Automation Profile.
     BuildingAutomation = 0x0105,
+
     /// Profile Identifier for Remote Control Profile.
     RemoteControl = 0x0107,
+
     /// Profile Identifier for Health Care Profile.
     HealthCare = 0x0108,
+
     /// Profile Identifier for Smart Energy Profile.
     SmartEnergy = 0x0109,
+
     /// Profile Identifier for Light Link Profile.
     TouchLink = 0xC05E,
 }
