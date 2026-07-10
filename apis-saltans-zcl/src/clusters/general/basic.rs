9370c93019dd22d4f5e60b6bb0d59023517d15b8
Richard Neumann <neumann@paulmann.de>
2026-07-10 17:48:40 +0200

    Add missing exports.

--- apis-saltans-zcl/src/clusters/general/basic.rs
+++ apis-saltans-zcl/src/clusters/general/basic.rs
@@ -1,6 +1,9 @@
 //! Zigbee Cluster Library (ZCL) Basic Cluster.
 
-pub use self::attributes::{Id, Readable, Reportable, Writable};
+pub use self::attributes::{
+    AlarmMask, DateCode, DisableLocalConfig, GenericDeviceClass, GenericDeviceType, Id, ParseError,
+    PhysicalEnvironment, PowerSource, Readable, Reportable, Writable,
+};
 pub use self::commands::{Command, ResetToFactoryDefaults};
 
 mod attributes;
