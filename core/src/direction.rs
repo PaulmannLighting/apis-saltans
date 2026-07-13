1520b3869d609edcda1454ec89327801ef89ea39
Richard Neumann <neumann@paulmann.de>
2026-07-13 21:08:08 +0200

    Allow parsing from `u8`.
    While at it, derive some more traits.

--- core/src/direction.rs
+++ core/src/direction.rs
@@ -1,6 +1,9 @@
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
 /// Frame direction.
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, FromPrimitive)]
 #[repr(u8)]
 pub enum Direction {
     /// Frame is sent from the client side to the server side.
@@ -8,4 +11,12 @@ pub enum Direction {
 
     /// Frame is sent from the server side to the client side.
     ServerToClient = 0x01,
+}
+
+impl TryFrom<u8> for Direction {
+    type Error = u8;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Self::from_u8(value).ok_or(value)
+    }
 }
