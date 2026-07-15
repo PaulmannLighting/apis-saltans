9a798933c2e7fff4e67b9add3329140e11e443b4
Richard Neumann <neumann@paulmann.de>
2026-07-15 10:31:56 +0200

    Implement displaying traits for `Status`.

--- zcl/src/status.rs
+++ zcl/src/status.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter, LowerHex, UpperHex};
+
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
@@ -12,48 +15,120 @@ mod deprecated;
 pub enum Status {
     /// Indicates the command was successful.
     Success = 0x00,
+
     /// Indicates the command failed.
     Failure = 0x01,
+
     /// Indicates the command is not authorized.
     NotAuthorized = 0x7e,
+
     /// Indicates the command was malformed.
     MalformedCommand = 0x80,
+
     /// Indicates the cluster command is not supported.
     UnsupportedCommand = 0x81,
+
     /// Indicates the field in the command is invalid.
     InvalidField = 0x85,
+
     /// Indicates the attribute is unsupported.
     UnsupportedAttribute = 0x86,
+
     /// Indicates the value of the attribute is invalid.
     InvalidValue = 0x87,
+
     /// Indicates the attribute is readable-only.
     ReadOnly = 0x88,
+
     /// Indicates there is insufficient space to perform the operation.
     InsufficientSpace = 0x89,
+
     /// Indicates the requested entry was not found.
     NotFound = 0x8b,
+
     /// Indicates the attribute is unreportable.
     UnreportableAttribute = 0x8c,
+
     /// Indicates the data type of the attribute is invalid.
     InvalidDataType = 0x8d,
+
     /// Indicates the selector is invalid.
     InvalidSelector = 0x8e,
+
     /// Indicates the command timed out.
     Timeout = 0x94,
+
     /// Indicates the command was aborted.
     Abort = 0x95,
+
     /// Indicates the image is invalid.
     InvalidImage = 0x96,
+
     /// Indicates the device is waiting for data.
     WaitForData = 0x97,
+
     /// Indicates no image is available.
     NoImageAvailable = 0x98,
+
     /// Indicates more image data is required.
     RequireMoreImage = 0x99,
+
     /// Indicates the notification is pending.
     NotificationPending = 0x9a,
+
     /// Indicates that the cluster is unsupported.
     UnsupportedCluster = 0xc3,
+}
+
+impl Status {
+    /// Returns a string representation of the status code.
+    #[must_use]
+    pub const fn as_str(self) -> &'static str {
+        match self {
+            Self::Success => "SUCCESS",
+            Self::Failure => "FAILURE",
+            Self::NotAuthorized => "NOT_AUTHORIZED",
+            Self::MalformedCommand => "MALFORMED_COMMAND",
+            Self::UnsupportedCommand => "UNSUP_COMMAND",
+            Self::InvalidField => "INVALID_FIELD",
+            Self::UnsupportedAttribute => "UNSUPPORTED_ATTRIBUTE",
+            Self::InvalidValue => "INVALID_VALUE",
+            Self::ReadOnly => "READ_ONLY",
+            Self::InsufficientSpace => "INSUFFICIENT_SPACE",
+            Self::NotFound => "NOT_FOUND",
+            Self::UnreportableAttribute => "UNREPORTABLE_ATTRIBUTE",
+            Self::InvalidDataType => "INVALID_DATA_TYPE",
+            Self::InvalidSelector => "INVALID_SELECTOR",
+            Self::Timeout => "TIMEOUT",
+            Self::Abort => "ABORT",
+            Self::InvalidImage => "INVALID_IMAGE",
+            Self::WaitForData => "WAIT_FOR_DATA",
+            Self::NoImageAvailable => "NO_IMAGE_AVAILABLE",
+            Self::RequireMoreImage => "REQUIRE_MORE_IMAGE",
+            Self::NotificationPending => "NOTIFICATION_PENDING",
+            Self::UnsupportedCluster => "UNSUPPORTED_CLUSTER",
+        }
+    }
+}
+
+impl Display for Status {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
+
+impl Error for Status {}
+
+impl LowerHex for Status {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        LowerHex::fmt(&(*self as u8), f)
+    }
+}
+
+impl UpperHex for Status {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        UpperHex::fmt(&(*self as u8), f)
+    }
 }
 
 impl From<Deprecated> for Status {
